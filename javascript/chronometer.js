79c4ac7c75b7307787bb7ef3962d07bf17703617
Tobias Conrad <max.tobias.conrad@gmail.com>
2022-06-21 20:09:05 +0200

    solved with bonus

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -2,16 +2,29 @@ class Chronometer {
   constructor() {
     this.currentTime = 0
     this.intervalId = null; 
+    this.mms = 0;
+    this.intervalIdMs = null;
   }
 
   start(printTimeCallback) {
-  
+    this.intervalIdMs = setInterval(() => {
+          if (this.mms > 99){
+         this.mms = 0;
+      }
+      this.mms++
+      if (typeof printTimeCallback === "function")  {
+        printTimeCallback()
+      }
+  }, 10)
     this.intervalId =  setInterval(() => {
         this.currentTime++ 
         if (typeof printTimeCallback === "function")  {
           printTimeCallback()
         }
     }, 1000)
+  }
+  getMilliseconds() {
+    return Math.floor(this.mms)
   }
 
   getMinutes() {
@@ -29,14 +42,16 @@ class Chronometer {
 
   stop() {
     clearInterval(this.intervalId)
+    clearInterval(this.intervalIdMs)
   }
 
   reset() {
     return this.currentTime = 0
+    
   }
 
   split() {
-      return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
+      return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}:${this.computeTwoDigitNumber(this.getMilliseconds()) }`
     }
 }
 
