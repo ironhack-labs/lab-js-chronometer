3289a6bbfe10fe652dabf36410938c1c89de9d73
Lisa Hilterhaus <lisa@hilterhaus.net>
2022-06-21 20:04:46 +0200

    Solve lab

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,75 @@
+
+//BONUS 9: Milliseconds (code in comments)
+
+
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+    // this.currentMilliseconds = 0;
+    // this.millisecIntervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+
+      if(typeof printTimeCallback === "function") {
+        printTimeCallback();
+      }
+    }, 1000);
+
+    // this.millisecIntervalId = setInterval(() => {
+    //   this.currentMilliseconds += 10; // --> adding 10 for timer accuracy (setInterval Documentation: delay is set to  10 if lower than 10)
+
+    //   if(typeof printTimeCallback === "function") {
+    //     printTimeCallback();
+    //   }
+    // }, 10);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
+    // return Math.floor((this.currentMilliseconds / 1000) / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
+    // return Math.floor((this.currentMilliseconds / 1000) % 60);
   }
 
+  // getMilliseconds() {
+  //   return Math.floor((this.currentMilliseconds % 1000 / 10));
+  // }
+
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let numberStr;
+
+    if (value < 10) {
+      return numberStr = `0${value}`;
+    }
+
+    return numberStr = `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    clearInterval(this.millisecIntervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.currentMilliseconds = 0;
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    // let milliseconds = this.computeTwoDigitNumber(this.getMilliseconds());
+    
+    return `${minutes}` + `:${seconds}` //+ `:${milliseconds}`
   }
 }
 
