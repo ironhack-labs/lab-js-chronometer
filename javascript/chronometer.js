e63c9206bef9f4ae08e252a4867de7d9ecb9a3ac
“Alexandre <coma.alexandre@gmail.com>
2022-06-21 16:16:09 +0200

    Lab solved

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,115 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (typeof printTimeCallback === "function") {
+        printTimeCallback();
+      };
+    },1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime%60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value === 0) {
+      return "00"
+    }
+    else if (value.toString().length === 1) {
+      return `0${value}`
+    }  
+    else {
+      return value.toString()
+    };
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
+
+
+// ******* Iteration 9 *********
+
+/* class Chronometer {
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
+    this.milliseconds = 0;
+    this.millisecondsId = null;
+  }
+
+  start(printTimeCallback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (typeof printTimeCallback === "function") {
+        printTimeCallback();
+      };
+    },1000);
+
+    this.millisecondsId = setInterval(() => {
+      this.milliseconds++;
+      if (typeof printTimeCallback === "function") {
+        printTimeCallback();
+      }
+    },1);
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime/60);
+  }
+
+  getSeconds() {
+    return this.currentTime%60;
+  }
+
+  getMilliseconds() {
+    return this.milliseconds
+  }
+
+  computeTwoDigitNumber(value) {
+    if (value === 0) {
+      return "00"
+    }
+    else if (value.toString().length === 1) {
+      return `0${value}`
+    }  
+    else {
+      return value.toString()
+    };
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+  }
+
+  reset() {
+    this.currentTime = 0;
+  }
+
+  split() {
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}:${this.computeTwoDigitNumber(this.getMilliseconds())}`
+  }
+}*/
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
