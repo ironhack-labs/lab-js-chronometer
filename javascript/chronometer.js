91cbc4d1a60ebf82c7f0b31f63def64831272727
Paul Tiry <tirypaul@gmail.com>
2022-06-21 16:51:43 +0200

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    const check = typeof printTimeCallback;
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (check === 'function') {
+        printTimeCallback();
+      }
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = this.currentTime / 6000;
+    return Number(Math.floor(minutes).toFixed(0));
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime / 100 - this.getMinutes() * 60;
+    return Number(Math.floor(seconds).toFixed(0));
+  }
+
+  getMilliseconds() {
+    const millis =
+      this.currentTime - this.getSeconds() * 100 - this.getMinutes() * 6000;
+    return Number(Math.floor(millis).toFixed(0));
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return value < 10 ? `0${value}` : `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const min = this.computeTwoDigitNumber(this.getMinutes()),
+      sec = this.computeTwoDigitNumber(this.getSeconds()),
+      milli = this.computeTwoDigitNumber(this.getMilliseconds());
+    return `${min}:${sec}:${milli}`;
   }
 }
 
