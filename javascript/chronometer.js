bb1b443869697faf627900668bb38e7416d99041
Lara Rockenstein <lara.rockenstein@live.de>
2022-06-21 18:22:23 +0200

    bonus solved but still one bug left for currentTime < 10s

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -10,15 +10,19 @@ class Chronometer {
       if (typeof printTimeCallback == 'function') {
       printTimeCallback(); 
       }
-    }, 1000);
+    }, 10);
   }
 
   getMinutes() {
-    return Math.floor(this.currentTime/60);
+    return Math.floor(this.currentTime/100/60);
   }
 
   getSeconds() {
-    return this.currentTime%60;
+    return this.currentTime/100%60;
+  }
+
+  getMilliseconds() {
+    return this.currentTime%100;
   }
 
   computeTwoDigitNumber(value) {
