ddbe4e45eb53ea05f6a3a7885b52d8068da9aa4b
heliodcf <heliodcf@gmail.com>
2022-07-11 14:19:41 -0400

    Done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime +=1;
+      if (typeof printTimeCallback === 'function') {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = 0;
+    minutes = Math.floor(this.currentTime / 60)
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let second = 0;
+    second = this.currentTime %60;
+    return second;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const stringValue = value.toString();
+    let strFinal = ("0" + stringValue).slice(-2);
+
+      return strFinal;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let min = this.computeTwoDigitNumber(this.getMinutes());
+    let sec = this.computeTwoDigitNumber(this.getSeconds());
+    
+    return `${min}:${sec}`;
   }
 }
 
