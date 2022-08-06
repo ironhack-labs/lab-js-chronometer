a488cbd56d87136457b2edb567bd46bff68786e4
Patrick-Groot <patrick@candis.io>
2022-08-06 14:45:07 +0200

    add Chronometer (incl Bonus)

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,46 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+  start(printTimeCallback){
+      this.intervalId = setInterval(() => {
+        this.currentTime += 1;
+        if (printTimeCallback) {
+          printTimeCallback()
+        } 
+      }, 10);
+  }  
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 6000);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime % 6000) / 100);
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 6000) % 100;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return value.toString().padStart(2,"0");
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let splitMin = this.computeTwoDigitNumber(this.getMinutes());
+    let splitSec = this.computeTwoDigitNumber(this.getSeconds());
+    let splitCen = this.computeTwoDigitNumber(this.getCentiseconds());
+    return `${splitMin}:${splitSec}.${splitCen}`;
   }
 }
