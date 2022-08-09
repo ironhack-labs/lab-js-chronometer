d63052973164105ee29f257eeb120998ad2d06a7
Eurípedes <euripedes.naves@outlook.com>
2022-08-09 17:41:30 -0300

    Finished without Bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,44 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+   this.intervalId = setInterval(() => {
+   this.currentTime += 1;
+    if (typeof printTimeCallback === 'function'){
+      printTimeCallback()
+    }
+   }, 1000)
+
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/60, 0)
   }
 
   getSeconds() {
     // ... your code goes here
+    return Math.floor(this.currentTime % 60, 0)
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return value < 10 ? `${("0" + value).slice(-2)}`: `${value}`
   }
 
   stop() {
     // ... your code goes here
+    return clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    return this.currentTime = 0
   }
 
   split() {
