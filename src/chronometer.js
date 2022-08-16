cc05b079331d3cf8bd14d158003da845580a092b
Lenis Cervantes <leniserik@gmail.com>
2022-08-16 20:10:16 +0200

    Chrono completed with bonus 7

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime = 0,
+    this.intervalId = null
+    
+  } 
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+
+
+      this.currentTime++;
+      if (printTimeCallback) {
+
+        printTimeCallback()
+      }
+
+
+    }, 1000);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor((this.currentTime / 60))
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    let secs = this.currentTime % 60
+    return +secs.toFixed(0)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
+    let valueString = value.toString()
+    if (valueString.length === 1){
+      return '0'+ valueString 
+    }
+    else return valueString
+
   }
 
   stop() {
-    // ... your code goes here
+   clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+
+    return (`${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`)
   }
 }
