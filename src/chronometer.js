0d1a97f9712cec27b3a2ebfcaa2031fec1f3369c
juca25 <jserranocucarella@gmail.com>
2022-08-16 17:05:44 +0200

    done failing bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,45 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
+
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+     this.intervalId = setInterval(() => {
+      this.currentTime++
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    // let seconds = currentSec - (minutes * 60)
+    return Math.floor(this.currentTime %60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ('00' + value).slice(-2)
   }
 
   stop() {
-    // ... your code goes here
+     clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+  let mins = this.computeTwoDigitNumber(this.getMinutes())
+  let secs = this.computeTwoDigitNumber(this.getSeconds())
+  console.log (`${mins}:${secs}`)
+  return `${mins}:${secs}`
+  
   }
 }
