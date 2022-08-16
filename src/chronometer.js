9faa885aeae49d61461ded1414561a4043561fa5
Your Name <you@example.com>
2022-08-16 20:13:17 +0200

    solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+
+      if (printTimeCallback) { //No comprendo bien por qué hay que hacer esto para que Jasmine me acepte la condición.
+        printTimeCallback();
+      }
+
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = this.currentTime / 60
+    return Math.floor(minutes)
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60
+    return Math.floor(seconds)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let numberToString = value.toString()
+    if (numberToString.length === 1) {
+      return "0" + numberToString; // Al ser un return no hace falta un else, porque no sigue bajando si la condición se da.
+    }
+    return numberToString
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let formatedMinutes = this.computeTwoDigitNumber(this.getMinutes())
+    let formatedSeconds = this.computeTwoDigitNumber(this.getSeconds())
+    let formatedTime = `${formatedMinutes}:${formatedSeconds}`
+    return formatedTime
   }
 }
