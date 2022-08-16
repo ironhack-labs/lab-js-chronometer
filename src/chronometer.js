5b25893280e82aa79074894cecc38ed1c8d62c56
Guzman <guzpdl@gmail.com>
2022-08-16 19:56:59 +0200

    [MAD 0822] Guzman Ponce de Leon

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,46 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval( ()=>  {
+      this.currentTime++  
+      if(printTimeCallback !=undefined){
+        printTimeCallback()
+      }
+        
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
-
+  
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime%60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    let newValue = String(value)
+
+    if(newValue.length < 2){
+      return '0'+newValue
+    } 
+    return newValue;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+  this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return (this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds()))  
   }
 }
