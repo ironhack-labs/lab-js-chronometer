a8cb1f8338a71f81c7bd5f226537561629f43cc1
Hector <villaescusahector@gmail.com>
2022-08-16 20:36:23 +0200

    Lab complete

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,90 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
+
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+
+      this.currentTime++;
+
+      if (printTimeCallback) {
+        printTimeCallback()
+
+      }
+    }, 1000);
+
   }
 
   getMinutes() {
     // ... your code goes here
+
+    let totalMinutes = this.currentTime / 60
+
+    return Math.floor(totalMinutes)
+
   }
 
   getSeconds() {
     // ... your code goes here
+
+    let totalSeconds = this.currentTime % 60
+
+    return totalSeconds
+
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+
+    let decimals = value.toString()
+
+    if (decimals.length == 1) {
+      return '0' + decimals
+    }
+
+    return decimals
+
   }
 
   stop() {
     // ... your code goes here
+
+    let pause = clearInterval(this.intervalId)
+
+    return pause
+
   }
 
   reset() {
     // ... your code goes here
+
+    let retry = this.currentTime = 0
+
+    return retry
+
   }
 
   split() {
     // ... your code goes here
+
+
+
+
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let getSeconds = this.computeTwoDigitNumber(this.getSeconds())
+
+    return `${minutes}:${getSeconds}`
+
+
+
+
+
+
+
+
   }
 }
