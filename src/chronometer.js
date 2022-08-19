12cdfaf9e6f081e10d859be2d8fe093f8dbacc49
Miriam Poletti <polettimiriam@gmail.com>
2022-08-19 11:40:01 +0200

    minutes and seconds done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,55 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+      return Math.floor(this.currentTime / 60)    
   }
 
   getSeconds() {
-    // ... your code goes here
+      return (this.currentTime % 60)
+  }
+
+  getCentiseconds() {
+    // count the centiseconds
+    // capture the centiseconds when split time
+    // reset to 0
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return ('0' + value.toString())
+    }
+    return value.toString()
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    // return a string where time is formatted as mm:ss
+    // the split() chan use previously declared methods getMinutes, getSeconds, computeTwoDigitNumber
+    let splitMin = this.computeTwoDigitNumber(this.getMinutes())
+    let splitSec = this.computeTwoDigitNumber(this.getSeconds())
+
+    return splitMin + ':' + splitSec
   }
+
 }
