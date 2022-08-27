2b6415e29325c42fd9a52c46dfd50e5266fc68e9
Ivan Rubio <smashedpurse@gmail.com>
2022-08-27 16:37:31 -0500

    Exercise complete

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime=0;
+    this.intervalId=null;
   }
 
+  
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if(printTimeCallback){
+        printTimeCallback();
+       }
+     },1000); 
+    }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor( this.currentTime/60)
+
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime %60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value<10){
+      return "0" + value;
+    }
+    return ""+ value
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0
   }
 
   split() {
-    // ... your code goes here
-  }
+    let minutos = this.getMinutes();
+    let segundos = this.getSeconds();
+
+    let minutosFormateados = this.computeTwoDigitNumber(minutos);
+    let segundosFormateados = this.computeTwoDigitNumber(segundos);
+    return minutosFormateados + ":" + segundosFormateados
+}
+
 }
