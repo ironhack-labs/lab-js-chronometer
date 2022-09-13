34a5482ec25d69c12306a2a6d00ebae8791d78cc
Saskia Peach <saskiapeach@gmail.com>
2022-09-13 15:45:47 +0100

    completed iterations 1-4

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,33 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(
+      () => {
+        this.currentTime += 1;
+    if(typeof printTimeCallback === 'function'){
+      printTimeCallback()
+    }
+      }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
 
   getSeconds() {
-    // ... your code goes here
+    const extraSeconds = this.currentTime % 60;
+    return extraSeconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const twoDigits = "";
+    twoDigits = this.currentTime.slice(0);
+    return twoDigits;
   }
 
   stop() {
