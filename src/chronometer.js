515ee32420d9645bd75a41bf0be1023bb745d0f2
Michel Pomares <mapomarest@gmail.com>
2022-09-13 18:45:22 +0200

    Cleaning some errors

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,64 +1,52 @@
 class Chronometer {
   constructor() {
-   this.currentTime = 0;
-   this.intervalId = null;   
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(printTimeCallback) {    
-    
-    this.intervalId = setInterval( () => {
+  start(printTimeCallback) {
+    this.intervalId = setInterval(() => {
       this.currentTime++;
-      if(typeof printTimeCallback === "function"){
+      if (typeof printTimeCallback === "function") {
         printTimeCallback();
       }
-
-    }, 1000)
-
+    }, 1000);
   }
 
   getMinutes() {
-
-    let minutes = this.currentTime/60;
-    return Math.floor(minutes);   
-
+    let minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
 
   getSeconds() {
-
-    let seconds = this.currentTime%60;
+    let seconds = this.currentTime % 60;
     return seconds;
-
   }
 
   computeTwoDigitNumber(value) {
     let stValue = value.toString();
-    if(value < 10){
+    if (value < 10) {
       return "0" + stValue;
-    }else if(value < 100){
+    } else if (value < 100) {
       return stValue;
-    }        
-
+    }
   }
 
   stop() {
-
     clearInterval(this.intervalId);
-
   }
 
   reset() {
-
     this.currentTime = 0;
-
   }
 
-
   split() {
-   
-    let minutes = this.getMinutes;
-    let seconds = this.getSeconds;
-    let minAndSec = `${computeTwoDigitNumber(minutes)}:${this.computeTwoDigitNumber(seconds)}`;
-    return minAndSec;    
+    let minutes = this.getMinutes();
+    let seconds = this.getSeconds();
+    let minAndSec = this.computeTwoDigitNumber(minutes) + ":" + this.computeTwoDigitNumber(seconds);
+    console.log(minAndSec);
+
+    return minAndSec;
 
   }
 }
