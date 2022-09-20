07d090f62a0f36db1a5fa6f61a98cd149f2c8beb
Yabel <yabelrr@gmail.com>
2022-09-20 15:01:31 +0200

    Iteration 3 & 4 DONE

--- src/chronometer.js
+++ src/chronometer.js
@@ -14,14 +14,20 @@ class Chronometer {
 
     }
     }, 1000);
+
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/ 60);
+
   }
 
   getSeconds() {
     // ... your code goes here
+    
+    return Math.floor(this.currentTime) % 60;
+
   }
 
   computeTwoDigitNumber(value) {
