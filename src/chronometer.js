2cf007114cdfe6f7350880c2dada8c848c000849
Andrea Leonetti <ciroandrealeonetti@gmail.com>
2022-10-10 16:35:51 +0100

    solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -44,6 +44,6 @@ return `0${value}`;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}` + ":" + `${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
