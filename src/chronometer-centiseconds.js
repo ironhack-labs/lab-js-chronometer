04e44e2f4993666d8a72236df8a62398f5c2c92b
Diana Pirlici <pirlicid@gmail.com>
2022-10-10 17:12:48 +0100

    final-lab

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime +=1;
+      if(printTimeCallback) 
+        printTimeCallback();
+      
+    },10)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor((this.currentTime)/6000);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(((this.currentTime)/100)%60);
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime)%100);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ('0' + value).slice(-2);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+   this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let min =this.computeTwoDigitNumber(this.getMinutes());
+    let sec =this.computeTwoDigitNumber(this.getSeconds());
+    let centisec = this.computeTwoDigitNumber(this.getCentiseconds());
+
+    let result = `${min}:${sec}.${centisec}`;
+    return result;
+
   }
 }
