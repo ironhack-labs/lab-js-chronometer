f6e26b308879e0d7a84955a4b9149c163578253c
Carolina Barbosa <carolina.bs.ribeiro@gmail.com>
2022-10-15 15:36:17 -0300

    lab8

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1
+      if(printTimeCallback) printTimeCallback()
+      console.log (this.currentTime)
+    }, 1000)
+
   }
 
   getMinutes() {
-    // ... your code goes here
-  }
+    return Math.floor(this.currentTime/60)
+    }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value<10) return `0${value}`
+    return `${value}`
   }
 
   stop() {
-    // ... your code goes here
+
+    return clearInterval(this.intervalId)
+    
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    // iteração 1
+    //retornar a string no time em que está
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
+    
   }
+
 }
+
+
