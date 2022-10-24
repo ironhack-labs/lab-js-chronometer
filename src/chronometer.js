2995738154f3a058274a2c516c87cd48038cb61a
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2022-10-24 18:15:42 -0500

    El tiempo es oro, por eso hay que cronometrar

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,69 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+     this.currentTime = 0;
+     this.intervalId = null;
   }
+  
 
   start(printTimeCallback) {
-    // ... your code goes here
+    let i = 1;
+    if(printTimeCallback == 'printTimeCallback'){
+      this.intervalId = setInterval(() => {
+        this.currentTime = i;
+        console.log(i);
+        i++;
+      },1000)
+    }else{
+      this.intervalId = setInterval(() => {
+        this.currentTime = i;
+        i++;
+      },1000)
+    }  
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = 0;
+    if(this.currentTime > 0){
+      minutes = Math.trunc(this.currentTime/60)
+    }else if(this.currentTime >= 0 && this.currentTime < 60){
+      return this.currentTime;
+  }
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let second = 0;
+    if(this.currentTime > 0){
+      second = this.currentTime%60;
+    }
+    return second;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let result = '00';
+    if (value < 10 && value >= 0){
+      return result = '0'+ value.toString();
+    }else if(value >= 10 && value <= 60){
+      return value.toString()
+    }
+    return result;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let min = this.getMinutes();
+    let sec = this.getSeconds();
+    let format = '00:00';
+    if(min > 0 || sec > 0){
+      return format = this.computeTwoDigitNumber(min)+':'+this.computeTwoDigitNumber(sec);
+    }
+    return format;
   }
 }
