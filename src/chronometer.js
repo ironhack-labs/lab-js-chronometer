339908924a20b2c1389e8e6ddf44016f6b5849fb
Julio Villalvazo Carrera <juliovillalvazo26@gmail.com>
2022-10-25 12:58:50 -0400

    Solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,58 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+    constructor() {
+        // ... your code goes here
+        this.currentTime = 0;
+        this.intervalId = null;
+    }
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+    start(printTimeCallback) {
+        // ... your code goes here
+        this.intervalId = setInterval(() => {
+            this.currentTime++;
+            if (printTimeCallback) {
+                printTimeCallback();
+            }
+        }, 1000);
+    }
 
-  getMinutes() {
-    // ... your code goes here
-  }
+    getMinutes() {
+        // ... your code goes here
+        return Math.floor(this.currentTime / 60);
+    }
 
-  getSeconds() {
-    // ... your code goes here
-  }
+    getSeconds() {
+        // ... your code goes here
+        return this.currentTime % 60;
+    }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    computeTwoDigitNumber(value) {
+        // ... your code goes here
+        const formattedValue = '' + value;
 
-  stop() {
-    // ... your code goes here
-  }
+        return formattedValue.length < 2
+            ? `0${formattedValue}`
+            : formattedValue;
+    }
 
-  reset() {
-    // ... your code goes here
-  }
+    stop() {
+        // ... your code goes here
+        clearInterval(this.intervalId);
+    }
 
-  split() {
-    // ... your code goes here
-  }
+    reset() {
+        // ... your code goes here
+        this.currentTime = 0;
+    }
+
+    split() {
+        // ... your code goes here
+        console.log(
+            `${this.computeTwoDigitNumber(
+                this.getMinutes()
+            )}:${this.computeTwoDigitNumber(this.getSeconds())}`
+        );
+        return `${this.computeTwoDigitNumber(
+            this.getMinutes()
+        )}:${this.computeTwoDigitNumber(this.getSeconds())}`;
+    }
 }
