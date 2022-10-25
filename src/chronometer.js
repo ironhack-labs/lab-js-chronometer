9c512b4abd5a31eb2a08383a112b6412a5bfa03f
Ezequiel Benitez <ezequielbv@gmail.com>
2022-10-25 16:33:23 +0200

    [BER OCT22 FT] - Ezequiel Done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    const intervalId = setInterval( () => {
+      this.currentTime += 1;
+
+      if(printTimeCallback) {
+        printTimeCallback();
+      }
+
+    }, 1000);
+
+    this.intervalId = intervalId;
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime%60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let string = "";
+    if( value.toString().length === 1 ){
+      string = '0' + value;
+    } else {
+      string = value.toString();
+    }
+    return string;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let min = this.computeTwoDigitNumber(this.getMinutes());
+    let sec = this.computeTwoDigitNumber(this.getSeconds());
+    return min + ":" + sec;
   }
 }
