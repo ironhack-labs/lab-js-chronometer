16bef5e9ea4d046d4638f575755ca5fa6fcd5b12
sijincodes <sijinmathews03@gmail.com>
2022-10-25 16:48:54 +0200

    wohoo, this was not as bad as I thought

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,56 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
+    if (printTimeCallback)
+      this.intervalId = setInterval(() => {
+        printTimeCallback();
+      }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    if (!this.currentTime) return 0;
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let stringValue = String(value);
+    if (stringValue.length == 1) return 0 + stringValue;
+    else return stringValue;
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return (
+      this.computeTwoDigitNumber(this.getMinutes()) +
+      ":" +
+      this.computeTwoDigitNumber(this.getSeconds())
+    );
   }
 }
