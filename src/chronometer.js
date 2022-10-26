e4a34d306d3beee13816961bbf616d4fefaac91c
Leslie <leslie@iMac-de-Leslie.local>
2022-10-25 20:48:41 -0500

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,54 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
-  }
+      this.intervalId = setInterval(()=>{
+        this.currentTime ++
+        console.log(printTimeCallback)
+        if(printTimeCallback){
+          printTimeCallback()
+        }
+      }, 1000)
+    }
+    
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
     // ... your code goes here
-  }
+    return this.currentTime%60
+    }
+  
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let value1 = String(value)
+    if(value1.length < 2){
+      return value1 = "0" + value1
+    }
+    return value1
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
