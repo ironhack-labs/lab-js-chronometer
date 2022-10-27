83dc4f9d2c58236f55163d463d136289567d0fdd
Catalina Sabogal <csabogalortiz@gmail.com>
2022-10-27 09:20:41 +0200

    Final Chronometer

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,53 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
 
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000);
+
+  }
   getMinutes() {
-    // ... your code goes here
+    let minutes = this.currentTime / 60
+    return Math.floor(minutes)
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60
+    return Math.floor(seconds)
+
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let numbers = value.toString()
+    if (numbers.length < 2) {
+      return '0' + numbers
+    } else {
+      return numbers
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let seconds = this.computeTwoDigitNumber(this.getSeconds())
+    return minutes + ':' + seconds
   }
 }
+
+
