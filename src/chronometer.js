f044088054edce479d096799b1eb306fc59248fb
Nicole <92593875+ozzleme@users.noreply.github.com>
2022-11-14 18:22:34 +0530

    complete except for start and split methods

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,38 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    printTimeCallback = () => this.currentTime++;
+    this.intervalId = setInterval(() => printTimeCallback(), 1000)
+    // why does should invoke the passed argument every one second work if I remove the this 
   }
 
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value >= 10) return value.toString();
+    if (value < 10) return "0" + value.toString();
+    if (value == 0) return value.toString() + "0";
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+   
   }
 }
