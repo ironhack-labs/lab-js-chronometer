31b025753414a290617962f6905c3a63fd2ac5d6
Carlos Sosa <csosa@simpletg.com>
2022-11-21 19:28:24 -0500

    lab - solved

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,57 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) printTimeCallback();
+      this.currentTime += 1;
+    }, 10);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 6000);
   }
 
   getSeconds() {
     // ... your code goes here
+    return Math.floor((this.currentTime / 100) % 60);
   }
 
   getCentiseconds() {
     // ... your code goes here
+    return this.currentTime % 100;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value.toString().length === 1) return '0' + value.toString();
+    return value.toString();
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return (
+      this.computeTwoDigitNumber(this.getMinutes()) +
+      ':' +
+      this.computeTwoDigitNumber(this.getSeconds()) +
+      '.' +
+      this.computeTwoDigitNumber(this.getCentiseconds())
+    );
   }
 }
