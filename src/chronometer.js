3e714e202e44d9ab2c79b26b88ad6c0fc2ef936e
Matt Hamilton <matt.hamilton49@gmail.com>
2022-11-29 19:08:05 +0100

    wasnt able to use the splice method or figure out how it would apply, was also  not able to fugure out how my function from iteration 5 could be used for iteration 8

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,71 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+   this.currentTime = 0;
+   this.intervalId = null; 
+  
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.timer = setInterval(() => {
+      this.currentTime += 1;
+      
+      if (typeof printTimeCallback === 'function') {
+        printTimeCallback();
+      }
+
+    }, 1000);
+
   }
 
   getMinutes() {
     // ... your code goes here
+    
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      return Math.floor(this.currentTime / 60);
+    }
+
   }
 
   getSeconds() {
     // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      return this.currentTime % 60;
+    }
+    
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    
+    if (value < 10) {
+      return `0${value.toString()}`;
+    } else {
+      return value.toString();
+    }
+    
+
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.timer);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    this.computeTwoDigitNumber()
+   
+    
   }
 }
