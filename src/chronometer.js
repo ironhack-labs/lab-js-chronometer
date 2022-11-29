5451460880fa61e6327997fbf367a3678ba00932
Laura Bährs <laura.baehrs@googlemail.com>
2022-11-29 16:47:59 +0100

    finish fourth iteration

--- src/chronometer.js
+++ src/chronometer.js
@@ -19,7 +19,8 @@ class Chronometer {
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = Math.floor(this.currentTime) % 60;
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
