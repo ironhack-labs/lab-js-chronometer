e7077234d6b970cc6625aa21d9feb6eea7a92d47
juliachernik <116951695+juliachernik@users.noreply.github.com>
2022-11-29 20:35:25 +0100

    Finished the lab with bonuses 8 and 9

--- src/chronometer.js
+++ src/chronometer.js
@@ -5,30 +5,40 @@ class Chronometer {
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if(printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+   return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value > 9) {
+    return `${value}`
+    }
+    if (value < 10) {
+      return "0".concat(value.toString());
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+   this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber (this.getMinutes()) + ":" + this.computeTwoDigitNumber (this.getSeconds());
   }
-}
+  }
