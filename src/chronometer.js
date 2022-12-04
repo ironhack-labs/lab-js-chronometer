b38130f56c1b0e334d742e499a0e4f5e399e821e
Victor Lopez <victorlpz2301@gmail.com>
2022-12-04 17:16:06 +0100

    starting

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,29 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000);
   }
+  
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
   }
 
   stop() {
