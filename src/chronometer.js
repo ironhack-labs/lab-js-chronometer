065a83723a8adc4c0a08d928307b4daccfb1f122
leandropereyradev <leandro-pereyra@hotmail.es>
2022-12-05 14:05:01 +0100

    Done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      printTimeCallback ? printTimeCallback() : null;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const splited = value.toString().split("");
+
+    return splited.length === 1 ? `0${splited.join("")}` : splited.join("");
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const mins = this.computeTwoDigitNumber(this.getMinutes());
+    const secs = this.computeTwoDigitNumber(this.getSeconds());
+    const cent = this.computeTwoDigitNumber(this.getCentiseconds());
+
+    return `${mins}:${secs}:${cent}`;
   }
 }
