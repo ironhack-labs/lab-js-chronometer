73905de0ef02c60c552401d44d491debc299e148
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@iMac-de-Ana.home>
2022-12-07 12:12:17 +0100

    solved lab up until bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -5,30 +5,37 @@ class Chronometer {
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval( () => {
+      this.currentTime++;
+      if(printTimeCallback) {
+      printTimeCallback();
+      }
+    },1000) 
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
+
+  // the instructions suggest using the .slice() method for this, and I don't see why or how to use it here. 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return value.toString().length === 1 ? `0${value.toString()}` : value.toString()
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
