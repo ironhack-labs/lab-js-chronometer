f5d9cb977fe39467db951da870f249e1cc71b911
physiodevapp <ficapp2.0@gmail.com>
2022-12-09 17:41:51 +0100

    feat: Iteration 2

--- src/chronometer.js
+++ src/chronometer.js
@@ -7,6 +7,12 @@ class Chronometer {
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
