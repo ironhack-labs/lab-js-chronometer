54e3a9846ed4d5de3b9cdc5bfb1b53bac731223d
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-04 17:09:20 +0100

    Add iteration 8

--- src/chronometer.js
+++ src/chronometer.js
@@ -35,10 +35,12 @@ class Chronometer {
 	}
 
 	reset() {
-		// ... your code goes here
+		this.currentTime = 0;
 	}
 
 	split() {
-		// ... your code goes here
+		let minutes = this.computeTwoDigitNumber(this.getMinutes());
+		let seconds = this.computeTwoDigitNumber(this.getSeconds());
+		return `${minutes}:${seconds}`;
 	}
 }
