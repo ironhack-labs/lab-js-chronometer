8d00b8a966d210f3849749bead591ff2c75be6d6
Subarna Paul Vignarajah <subarnapaul@rocketmail.com>
2023-01-07 21:20:19 +0000

    Solved Lab

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime=0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval( () => { 
+      if(printTimeCallback) {
+        printTimeCallback();
+      } 
+      this.currentTime++;
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
-  }
+    return Math.floor(this.currentTime/(60*100))  }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/100)%60
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return (this.currentTime -(this.getMinutes()*1000))%100
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value.toString().length === 2){
+      return value.toString()
+    }
+    else {
+      return '0'+value.toString()
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}.${this.computeTwoDigitNumber(this.getCentiseconds())}`
   }
 }
+
+let ch1 = new Chronometer()
+
+ch1.currentTime = 1725
+console.log(ch1.split());
