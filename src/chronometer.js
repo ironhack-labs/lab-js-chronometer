7ff868212300b88735499b5189aeb7db3e61fbcb
BodyElvis <bodymoliki@gmail.com>
2023-01-07 18:54:51 +0100

    Lab-Solution-Chronometer

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return "0" + value;
+    } else {
+      return value.toString().slice(-2);
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    this.running = false;
   }
 
   reset() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    this.currentTime = 0;
+    this.running = false;
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.getMinutes();
+    let seconds = this.getSeconds();
+    return this.computeTwoDigitNumber(minutes) + ":" + this.computeTwoDigitNumber(seconds);
   }
 }
