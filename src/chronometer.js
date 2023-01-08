9eb0dff2ac8e38418642a8f460694bf3eb79f9d9
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-01-08 17:50:11 +0100

    lab almost fully solved :D
    I am still catching up on the call backs and asynchronous functions.

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    const intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if ((value = typeof Number)) {
+      value.slice(0, 2).toString()
+    }
+    console.log(value)
+    return value
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
-  split() {
-    // ... your code goes here
-  }
+  split() {}
 }
+
+console.log(computeTwoDigitNumber())
