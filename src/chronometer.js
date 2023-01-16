45e7b72afa496a81ec7ab42e3df75475d6ebeece
Neilly <neilpilarca@gmail.com>
2023-01-16 16:54:31 +0100

    initial commit
    cd
    ecit
    exit
    '

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,10 +1,14 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.currentTime++;
+    setInterval(() => (this.currentTime = 3), 3000);
   }
 
   getMinutes() {
