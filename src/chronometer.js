cb44b38b436c647f96ff69f3b5d9aa834efb6577
Pedro Henrique <pedrohgbarbosa@hotmail.com>
2023-01-17 18:58:47 -0300

    Solved Lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,41 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    (this.currentTime = 0), (this.intervalId = null);
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) printTimeCallback();
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
+    return this.currentTime % 60;
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return value < 10 ? "0" + value : String(value);
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+
+    this.reset = this.currentTime = 0;
   }
 
   split() {
