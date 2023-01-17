d6ffd9b6b911989c79299fd38dfe43a33ed140c4
OFKIR Mohamed <mohamed_ofkir@outlook.fr>
2023-01-17 15:18:44 +0100

    first save

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,40 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+  this.intervalId = setInterval(() => {this.currentTime ++
+    if (printTimeCallback){ printTimeCallback()}
+  }, 1000);
   }
 
   getMinutes() {
+    return Math.floor(this.currentTime/60)
     // ... your code goes here
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let a = ''
+    if (value > 9 ) return a + value
+    else return '0' + value
+    
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
