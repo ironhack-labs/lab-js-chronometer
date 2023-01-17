262b444526f166212ed33dd879353b9ed310aff3
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-01-17 15:32:07 +0100

    iterations 3-8

--- src/chronometer.js
+++ src/chronometer.js
@@ -3,32 +3,40 @@ class Chronometer {
 this.currentTime = 0
 this.intervalId = null
   }
-
   start(printTimeCallback) {
-    // ... your code goes here
+    
   }
 
   getMinutes() {
-    // ... your code goes here
+  const minutes = Math.floor(this.currentTime/60)
+  return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if ( value < 10){
+      return '0' + value; 
+    }
+    return value.toString()
   }
 
   stop() {
-    // ... your code goes here
+  clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+  this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let minutes = getMinutes();
+    let seconds = getSeconds();
+    let splitMinutes = computeTwoDigitNumber(minutes);
+    let splitSeconds = computeTwoDigitNumber(seconds);
+    return splitMinutes + ':' + splitSeconds 
   }
 }
