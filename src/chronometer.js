346520510b5c2265b48319a1b84ddf6b8ae0b771
maxbrandt96 <max.brandtk@gmail.com>
2023-01-18 08:59:04 -0600

    FTMX012023-Max Brandt

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,29 +1,60 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
+  
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000);
+    if (printTimeCallback){
+      printTimeCallback
+    }
   }
 
   getMinutes() {
+    if (this.currentTime === 0){
+      return 0
+    }
+    else{
+      return Math.ceil(this.currentTime/6000)
+    }
     // ... your code goes here
   }
 
   getSeconds() {
     // ... your code goes here
+    if (this.currentTime === 0){
+      return 0
+    }
+    else{
+      return this.currentTime
+    }
   }
 
   computeTwoDigitNumber(value) {
+    if (this.currentTime < 10){
+      let twoDigit = "0" + value
+      return twoDigit
+    }
+    else if (this.currentTime > 9){
+      let twoDigit2 = this.currentTime
+      return twoDigit2
+    }
     // ... your code goes here
   }
 
   stop() {
+    clearInterval(this.intervalId)
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0
     // ... your code goes here
   }
 
