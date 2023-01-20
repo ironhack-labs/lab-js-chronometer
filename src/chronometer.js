c7a36093dcfc554f46336e4e879858e3dbe17e61
EmilianoCC <emiliano93cc@gmail.com>
2023-01-19 18:44:48 -0600

    Lab completo

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,51 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
+    if (this.currentTime === 0) return 0
+    return Math.floor(this.currentTime / 60)
     // ... your code goes here
   }
 
   getSeconds() {
+    if (this.currentTime === 0) return 0
+    return Math.floor(this.currentTime % 60)
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    return ('0' + value).slice(-2)
+
     // ... your code goes here
   }
 
   stop() {
+    clearInterval(this.intervalId)
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0
     // ... your code goes here
   }
 
   split() {
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let second = this.computeTwoDigitNumber(this.getSeconds())
+
+    return `${minutes}:${second}`
     // ... your code goes here
   }
 }
