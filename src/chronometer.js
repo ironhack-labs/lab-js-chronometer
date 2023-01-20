87dfde1181981287dcc98c8b39215cabe224ce0a
KARLALOPEZWDFTJAN2023 <karlacarbajalguillots@gmail.com>
2023-01-20 09:44:08 -0600

    advanced lav
    advance

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,30 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+   this.currentTime = 0
+   this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+   let startId = setInterval((printTimeCallback) => {
+    console.log(this.currentTime)
+    this.currentTime++
+   },1000)
+   console.log(startId)
   }
 
   getMinutes() {
     // ... your code goes here
+    return this.currentTime/60
+    Math.floor(this.getMinutes)
+
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime%60
+
   }
 
   computeTwoDigitNumber(value) {
@@ -21,10 +33,13 @@ class Chronometer {
 
   stop() {
     // ... your code goes here
+    clearInterval(this.currentTime)
+
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
