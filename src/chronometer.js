c0a0dc73f213f2ebd154db6eb967390a164faed1
Maria Fernanda Echeverria Colorado <fernanda.ecmf@gmail.com>
2023-01-22 19:34:20 -0600

    WDFT JAN2023Fernanda

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,62 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    }
+   currentTime = 0
+    intervalId = null
 
-  start(printTimeCallback) {
-    // ... your code goes here
+
+  start(printTimeCallback){
+    let currentTime = 0;
+
+function start(printTimeCallback) {
+  let currentTime = 0;
+
+function start(printTimeCallback) {
+    currentTime = 0;
+    let iidInterval = setInterval(() => {
+        currentTime++;
+        printTimeCallback(currentTime);
+    }, 3000);
+}
+}
   }
 
   getMinutes() {
-    // ... your code goes here
+   return Number(Math.ceil(this.currentTime/1000))
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime % 1000))
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+      let Value = value.toString();
+      if (Value.length === 1) {
+          Value = "0" + Value;
+      }
+      return Value;
   }
+
+      
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
-
   reset() {
-    // ... your code goes here
-  }
+    
+      this.currentTime = 0}
+    
 
-  split() {
-    // ... your code goes here
+  split() 
+  {
+    function getTimeFormat(totalSeconds) {
+      let minutes = Math.floor(totalSeconds / 60);
+      let seconds = totalSeconds % 60;
+      let minutesString = computeTwoDigitNumber(minutes);
+      let secondsString = computeTwoDigitNumber(seconds);
+      let time = minutesString + ":" + secondsString;
+      return time;
   }
+  console.log(getTimeFormat(60));
+  }  
 }
