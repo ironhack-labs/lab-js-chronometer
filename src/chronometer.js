92d8a4d891e7be09433dbc21a2c4acb98f77b677
ClaudiaKarla <115264070+ClaudiaKarla@users.noreply.github.com>
2023-01-23 11:44:59 -0600

    trabajo 95% terminado

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+  this.currentTime=0;
+  this.intervalId=null;
   }
-
+/////////////////////////////////////////////////////////////////
   start(printTimeCallback) {
-    // ... your code goes here
-  }
 
-  getMinutes() {
-    // ... your code goes here
+let contadorM=0;
+let contadorS=0;
+setInterval(()=> {
+   this.currentTime++
+    if(contadorS<3){
+    }  if( contadorM==0){
+
+    }
+   }, 1000);
   }
+/////////////////////////////////////////////////////////////
+getMinutes() {
+
+var resul=this.currentTime/60;
+var result=Math.floor(resul);
+return result;
+}
 
   getSeconds() {
-    // ... your code goes here
+   var result=this.currentTime%60;
+   return result;
+  
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    var str = value.toString(); 
+    if(value<10){
+      return 0+str ;
+  }else{
+      return str;
+  }
+
   }
 
   stop() {
-    // ... your code goes here
-  }
+        clearInterval(Chronometer)
+      
+      }
+  
+  
 
   reset() {
-    // ... your code goes here
+  this.currentTime=0;
   }
 
   split() {
-    // ... your code goes here
+   
+ let time= new Date(this.getMinutes()+ ":" +[this.getSeconds()]);
+ return time;
   }
-}
+  }
