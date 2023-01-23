ad76ffa57059e8d4e43337dd6f8fd39c2edbbbc0
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-01-23 08:49:59 +0100

    parcialmente hecho

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,46 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    
+    this.currentTime = 0;
+    this.intervalId = null;
+
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    
+       this.intervalId = setInterval (() => {
+
+          this.currentTime ++;
+
+          if (printTimeCallback){
+            printTimeCallback()
+          }
+
+      }, 1000)
+      
+     
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor (this.currentTime/60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60 ;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if(value.length === 1){
+      this.value1 = `0${value.slice(0,0)}`
+      return this.value1;
+    } else if(value.length === 2){
+       this.value2 = `${value.slice(0,1)}`
+      return this.value2;
+    }
   }
 
   stop() {
@@ -31,3 +55,5 @@ class Chronometer {
     // ... your code goes here
   }
 }
+
+
