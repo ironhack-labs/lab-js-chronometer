7e7348751753e10f5795c6e0d6ebaa6244bfa957
“Eveline <eveline-c@hotmail.com>
2023-01-23 16:36:33 +0000

    finishes all

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
-
+  this.intervalId = setInterval (() => {
+  this.currentTime++
+  if (printTimeCallback) return printTimeCallback()
+  }, 1000);
+}
   getMinutes() {
-    // ... your code goes here
+    if (!this.currentTime) return 0
+else {
+return Math.floor(this.currentTime / 60)
   }
+}
 
   getSeconds() {
-    // ... your code goes here
+    if (!this.currentTime) return 0
+    else {
+      return Math.floor(this.currentTime % 60)
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) return `0${value}`
+    else {
+      return `${value}`
+    }
   }
 
+
   stop() {
-    // ... your code goes here
+    clearInterval (this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
-  }
+    
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}` 
+  } // you need to write getMinutes () with parantheses bc it's a method/function
 }
+
