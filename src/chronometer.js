17e3c8d2b695ae09e212deab0b5b41fd1aed8d77
Assebe <Lambadina1>
2023-01-23 16:54:50 +0000

    Finished Lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,45 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+     this.intervalId = setInterval(() => { 
+      if(printTimeCallback){
+      return printTimeCallback()}
+      this.currentTime++  
+      }, 1000)
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    if(this.currentTime){const minutes = Math.floor(this.currentTime / 60)
+      return minutes}
+      else return 0
   }
 
   getSeconds() {
-    // ... your code goes here
+   return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value <10){
+    return "0" + value.toString()}
+    else return value.toString()
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+ this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+   const timeSinceStart = ""
+   
+
   }
 }
