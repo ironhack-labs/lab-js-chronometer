4e7b7aa8f88e418fb93f5f9b983336e9190a4820
deborahdelimaa <deborahftlima@gmail.com>
2023-01-23 15:36:20 +0000

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+  this.currentTime = 0;
+  this.intervalId = null;
+  }   
+  
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
-
+    this.intervalId = setInterval(() => {
+      console.log(this.currentTime);
+      this.currentTime++;
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+    }, 1000);
+  };
   getMinutes() {
-    // ... your code goes here
-  }
-
+    return Math.floor(this.currentTime/60);
+   }
   getSeconds() {
-    // ... your code goes here
+     return Math.floor(this.currentTime % 60);
   }
-
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
-
-  stop() {
-    // ... your code goes here
-  }
-
-  reset() {
-    // ... your code goes here
-  }
-
-  split() {
-    // ... your code goes here
+    if(value < 10){
+  return `0${value}`
+  } else {
+    return `${value}`;
   }
 }
+  stop() {
+    clearInterval(this.intervalId)
+  }
+  reset() {
+     return this.currentTime = 0;
+     }
+  
+  split() { 
+    if(this.computeTwoDigitNumber){
+      return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`;
+    }
+  
+  }
+}
+
+
+
+
