bd835a4276dc97260dfdba7362757082508e16ff
Daniel Yanez <ingdanielyanez@gmail.com>
2023-01-24 16:34:42 +0100

    [MAD PP 0123] ANDREI COSTEA - DANIEL YANEZ

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,26 +1,48 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) printTimeCallback()
+      // console.log(intervalId)
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    const entireMinutesPassed = Math.floor(this.currentTime / 60)
+
+    return entireMinutesPassed
   }
 
   getSeconds() {
     // ... your code goes here
+    const entireSecondsPassed = Math.floor(this.currentTime % 60)
+
+    return entireSecondsPassed
+
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const numToString = value.toString()
+    if (numToString.length === 1) {
+      return ('0' + numToString).slice(0, 2)
+    }
+    return numToString
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
+
   }
 
   reset() {
