d7fe0813a8c7f5e1118cde3ab9e68a65198324ed
jelloreda <jaimelloredavelasco@gmail.com>
2023-01-24 16:56:27 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,49 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
+
+    let minutes = Math.floor(this.currentTime / 60)
+
+    return minutes
   }
 
   getSeconds() {
     // ... your code goes here
+    let seconds = this.currentTime % 60
+
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let twoDigitNumber
+
+    if (value.length < 10) {
+      twoDigitNumber = Concat('0', value.numberAsString())
+    } else {
+      twoDigitNumber = value.toString()
+    }
+
+    console.log(typeof twtwoDigitNumbero);
+
+    return twoDigitNumber
   }
 
   stop() {
