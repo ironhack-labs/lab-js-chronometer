388b5f31ede7e769824bc20129402c2f2c9ddf80
Miguel Ferragut <mferracab@gmail.com>
2023-01-24 16:32:14 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,37 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
+
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      // printTimeCallback()
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+      this.currentTime++
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    const minutes = this.currentTime / 60
+    return (Math.floor(minutes))
   }
 
   getSeconds() {
     // ... your code goes here
+    const seconds = 0
+    if (this.currentTime % 60 === 0) {
+      seconds = 0
+    }
+    console.log(this.currentTime)
+    return this.currentTime
   }
 
   computeTwoDigitNumber(value) {
