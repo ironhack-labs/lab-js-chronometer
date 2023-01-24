3f784e2ca867f3d8738a55653deaf134b5e08136
Cristina Gutiérrez <cgutierrezgm@gmail.com>
2023-01-24 16:36:38 +0100

    v1

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,46 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+
+      this.currentTime++
+
+      if (printTimeCallback) printTimeCallback()
+
+    }, 1000)
+
   }
 
   getMinutes() {
     // ... your code goes here
+    let minutes = this.currentTime / 60
+
+    return Math.floor(minutes)
   }
+
 
   getSeconds() {
     // ... your code goes here
+    let seconds = this.currentTime % 60
+
+    return Math.floor(seconds)
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+
+    if (value < 10) {
+      return '0' + value.toString()
+    } else {
+      return value.toString()
+    }
+
   }
 
   stop() {
