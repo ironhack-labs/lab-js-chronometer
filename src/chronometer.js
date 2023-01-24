9dc009588405f40abd66917de2a29f2f3257d8bb
Danidm17 <danidm.cam@gmail.com>
2023-01-24 16:37:20 +0100

    Solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,14 +1,28 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
+
+
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+
+    const intervalId = setInterval(() => {
+
+      this.currentTime++
+
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+
+    }, 1000)
+    
   }
 
   getMinutes() {
-    // ... your code goes here
+
   }
 
   getSeconds() {
