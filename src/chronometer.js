ed8ee23c685edec32c9a84e54861ee5222596b8e
TheMike1972 <TheMike1972@github.com>
2023-01-24 18:50:07 +0100

    Michaël Biezin WD FT Paris JAN

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (typeof printTimeCallback === "function") {
+      printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime < 60) {
+      return 0; 
+    }
+    else if (this.currentTime >= 60) {
+      return Math.floor(this.currentTime/60);
+    }
   }
 
   getSeconds() {
-    // ... your code goes here
+      return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+ 
+ //   return value.toString().padStart(2, '0');
+ // ou
+  return `0${value}`.slice(-2);
   }
 
-  stop() {
-    // ... your code goes here
+// ou
+
+   stop() {
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
