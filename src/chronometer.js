8bd1e1a15a80fcae26be7445b4adf2bf8465eb5e
Natalia Nossack <nvcordeiro22@gmail.com>
2023-02-09 06:58:01 -0300

    Solved Lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,57 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    (this.currentTime = 0), (this.intervalId = null);
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    //setInterval(() => {/* */}).
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        return printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    let min = (this.currentTime / 60);
+  let integerMinutes = Math.floor(min);
+    return integerMinutes;
   }
 
   getSeconds() {
     // ... your code goes here
+    let sec = this.currentTime % 60
+    return sec;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value < 10) {
+      return `0${value}`
+    } 
+    else {
+      return '${value}'
   }
 
   stop() {
     // ... your code goes here
-  }
+    clearInterval(this.currentTime)
+  } 
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
+}
 }
