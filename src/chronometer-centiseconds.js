9df7bf379d5be59230151649026372b85d6b1dde
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-02-23 02:20:13 +0100

    Finished split method

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -55,6 +55,6 @@ class Chronometer {
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}.${this.computeTwoDigitNumber(this.getCentiseconds())}`
   }
 }
