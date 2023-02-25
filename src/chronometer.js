5a5014a35f0effa8a63ae9ddd8e21145f84cf4a8
Wiebke Lueneburg <git@fraulueneburg.de>
2023-02-25 14:40:56 +0100

    completed iteration 1 and 3-5

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,33 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    setTimeout(()=>{
+      this.currentTime += 1;
+    },1000)
+
+    // setInterval(()=>{
+    //   this.printTimeCallback
+    // },1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value >= 10) {
+      return value.toString();
+    } else {
+      return "0" + value;
+    }
   }
 
   stop() {
