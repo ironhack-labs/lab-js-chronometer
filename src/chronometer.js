5769be99f0edb6bc9fead5292f96f8faa10707c1
Michel Saber <michel-saber@hotmail.com>
2023-02-25 16:38:37 +0100

    Lab done / html changed for bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,46 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
-
+  //setInterval(function () {element.innerHTML += "Hello"}, 1000);
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
+    return this.intervalId;
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes(this.currentTime)
+    )}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}`;
   }
 }
