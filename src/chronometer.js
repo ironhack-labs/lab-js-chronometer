4369b36f36ec63d8ef423240853464f5a45bc172
Max Strandberg <maax.strandberg@gmail.com>
2023-02-25 18:46:50 +0100

    done and done done done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,63 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
+
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(()=>{this.currentTime += 1;
+      if (printTimeCallback){
+        printTimeCallback()
+      }
+      }, 1000);
+
   }
 
   getMinutes() {
+   return Math.floor(this.currentTime/ 60);
+
     // ... your code goes here
   }
 
   getSeconds() {
+    return this.currentTime % 60;
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let num = value.toString()
+    if (value <10){ 
+      
+      return `0` + num;
+    }else { 
+      return num;
+    }
+
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    
+     
+
+
+    // ... your code goes heres
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
+
+    let min= this.computeTwoDigitNumber(this.getMinutes());
+    let sec = this.computeTwoDigitNumber(this.getSeconds());
+    return  (min +`:`+ sec);
     // ... your code goes here
   }
+  
 }
