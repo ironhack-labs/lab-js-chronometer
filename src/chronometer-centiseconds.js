037fa64dfc9903a6fabff78d276472fbd3eb6fe1
“Zuzana <zuzana@getyourguide.com>
2023-02-25 16:36:02 +0100

    Completed

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0; // this is in centiseconds now
+    this.intervalId = null;
   }
-
+  //setInterval(function () {element.innerHTML += "Hello"}, 1000);
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 10);
+    return this.intervalId;
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 6000);
   }
 
   getSeconds() {
-    // ... your code goes here
-  }
-
-  getCentiseconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      return Math.floor((this.currentTime / 100) % 60);
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+  }
+
+  getCentiseconds() {
+    return this.currentTime % 100;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes(this.currentTime)
+    )}:${this.computeTwoDigitNumber(
+      this.getSeconds(this.currentTime)
+    )}.${this.computeTwoDigitNumber(this.getCentiseconds(this.currentTime))}`;
   }
 }
