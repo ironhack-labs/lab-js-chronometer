e5b236011b8b0e26d9e8d28444485e72568497da
Yvo de Rooij <110634893+YvodeRooij@users.noreply.github.com>
2023-02-25 16:18:56 +0100

    finished lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
+  // should return the number of entire minutes passed
   getMinutes() {
-    // ... your code goes here
+    const currentTimeMinutes = Math.floor(
+      this.currentTime / 60
+    );
+    return currentTimeMinutes;
   }
 
+  // should return the number of entire seconds passed
   getSeconds() {
-    // ... your code goes here
+    const currentTimeSeconds =
+      this.currentTime % 60;
+    console.log(currentTimeSeconds);
+    return currentTimeSeconds;
   }
 
+  // should return a string
+  // should always return a string of length 2
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let num = value.toString();
+    if (value < 10) {
+      return `0` + num;
+    } else {
+      return num;
+    }
   }
-
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let min = this.computeTwoDigitNumber(
+      this.getMinutes()
+    );
+    let sec = this.computeTwoDigitNumber(
+      this.getSeconds()
+    );
+    return min + `:` + sec;
   }
 }
