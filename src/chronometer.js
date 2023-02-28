25eecf336616385ba3d3992619141ea142d7a891
andrehcarvalho6 <andre.hcarvalho1@gmail.com>
2023-02-28 16:13:49 +0000

    i had trouble placing the two digit number, it only passes some of them

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,45 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
 
+  // another way of doing it
+  //return Math.floor(this.currentTime / 60);
+
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60;
+    return Math.floor(seconds);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value.length < 10) {
+      return null;
+    } else {
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
