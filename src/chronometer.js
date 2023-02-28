1bc8fa5d277ce5d8e765f2828d31376ea056db13
BigTogu <silvia.kenaan@gmail.com>
2023-02-28 12:54:50 +0100

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      ++this.currentTime;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let text = value.toString();
+    if (text.length === 1) {
+      text = "0" + value.toString();
+      // console.log(text);
+    }
+    return text;
   }
-
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.getMinutes();
+    let seconds = this.getSeconds();
+    return `${this.computeTwoDigitNumber(minutes)}:${this.computeTwoDigitNumber(
+      seconds
+    )}`;
   }
 }
