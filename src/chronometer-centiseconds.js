8e48d66a755e07b4457fe1783342bb41f2306a33
Alex Uceda <alexucedacintas@gmail.com>
2023-02-28 16:14:02 +0100

    Alex Chronometer

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,55 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    
+    this.intervalId = setInterval(()=>{
+      this.currentTime += 1;
+      if(printTimeCallback){
+        printTimeCallback();
+      }
+    }
+    ,10)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/6000);
   }
-
-  getSeconds() {
-    // ... your code goes here
-  }
+ 
+   getSeconds() {
+    return Math.floor((this.currentTime % 6000) / 100);
+   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 100);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const valueString = value.toString();
+    if(value<10){
+      return `0${valueString}`;
+    }
+    else{
+      return `${valueString}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const min = this.computeTwoDigitNumber(this.getMinutes())
+    const secs = this.computeTwoDigitNumber(this.getSeconds())
+    const centsecs = this.computeTwoDigitNumber(this.getCentiseconds())
+    return `${min}:${secs}.${centsecs}`;
   }
 }
+
