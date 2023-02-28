03ce939afca0d9c411edb4a003c928c5faf6764d
Mauricio Benavente <mauriciobenavente97@icloud.com>
2023-02-28 11:26:11 +0100

    iterations + split bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,64 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+      return printTimeCallback();
+    }
+      
+    }, 1000);
   }
+  
 
+
+  
+  
   getMinutes() {
-    // ... your code goes here
-  }
+      return Math.floor(this.currentTime / 60)
+
+    }
+  
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
+    
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+      let text = value.toString();
+      if (text.length === 1) {
+        text = "0" + value.toString();
+      }
+      return text;
+    }
+  
+
+
+
+  
+  
+
+
 
   stop() {
-    // ... your code goes here
+clearInterval(this.intervalId)
+    
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
+    
+  
+
 }
