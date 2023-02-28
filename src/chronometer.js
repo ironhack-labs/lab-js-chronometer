b6a0b9532a9a5b46e7568f2816ff12cb79512567
valyy151 <marin.valenta1@gmail.com>
2023-02-28 11:44:31 +0100

    Initial Commit

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,34 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+	constructor() {
+		this.currentTime = 0;
+		this.intervalId = null;
+	}
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+	start(printTimeCallback) {
+		// ... your code goes here
+	}
 
-  getMinutes() {
-    // ... your code goes here
-  }
+	getMinutes() {
+		// ... your code goes here
+	}
 
-  getSeconds() {
-    // ... your code goes here
-  }
+	getSeconds() {
+		// ... your code goes here
+	}
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+	computeTwoDigitNumber(value) {
+		// ... your code goes here
+	}
 
-  stop() {
-    // ... your code goes here
-  }
+	stop() {
+		// ... your code goes here
+	}
 
-  reset() {
-    // ... your code goes here
-  }
+	reset() {
+		// ... your code goes here
+	}
 
-  split() {
-    // ... your code goes here
-  }
+	split() {
+		// ... your code goes here
+	}
 }
