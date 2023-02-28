a5e97073e12c096e0b277720f3faa310762ade01
Hugo Silva <dasilvapapis@protonmail.com>
2023-02-28 23:15:49 +0000

    9 failures to fix

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,29 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+    
   }
 
+  //Iteration 2: The start method
   start(printTimeCallback) {
-    // ... your code goes here
+    
+    
+    this.intervalId = setInterval(() => {
+      this.currentTime = this.currentTime + 1;
+      if (typeof printTimeCallback === "function") {
+        printTimeCallback();
+      }
+    }, 1000);
+    
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.round(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
