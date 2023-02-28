ff47843c20e1188cfa6dedef0e030389f28090d9
Pim Blom <pim.blom@icloud.com>
2023-02-28 22:28:36 +0100

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,45 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback){
+        printTimeCallback();
+      }
+    }, 1000);
+    
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor((this.currentTime/60))
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime%60))
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let string= value.toString()
+    if (string.length<2){
+      string= "0"+string.slice(0)
+    }
+    return string
+
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0
   }
 
   split() {
-    // ... your code goes here
+    this.getMinutes() this.getSeconds
   }
 }
