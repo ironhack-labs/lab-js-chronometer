a95bb586848dc1bfb026f9a61845aefb7c053d33
drose123456 <acg_danial@hotmail.com>
2023-02-28 16:22:45 +0000

    all iterations complete aside bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,65 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (printTimeCallback) {
+      this.intervalId = setInterval(() => {
+        this.currentTime += 1;
+        printTimeCallback();
+      }, 1000);
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime += 1;
+      }, 1000);
+    }
   }
 
-  getMinutes() {
-    // ... your code goes here
+ getMinutes() {
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    return Math.floor(this.currentTime / 60);
   }
 
-  getSeconds() {
-    // ... your code goes here
+ getSeconds() {
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    return this.currentTime % 60;
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+computeTwoDigitNumber(value) {
+  // If the value is 0, return '00'
+  if (value === 0) {
+    return '00';
   }
+  
+  // Convert the value to a string
+  let strValue = value.toString();
+  
+  // If the string is only 1 character long, add a leading 0
+  if (strValue.length === 1) {
+    strValue = '0' + strValue;
+  }
+  
+  // Return the string, which will always be 2 characters long
+  return strValue;
+}
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
-  reset() {
-    // ... your code goes here
+ reset() {
+    this.currentTime = 0;
   }
 
-  split() {
-    // ... your code goes here
+split() {
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
