d5b87cbff93d8164ab76de00b1c78f952a1b0c03
SalvadorBellver <salvador@salvadorbellver.com>
2023-03-05 15:29:42 +0100

    Update chronometer.js
    done!

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,56 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId = null;
+    }
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+    start(printTimeCallback) {
 
-  getMinutes() {
-    // ... your code goes here
-  }
+        if (printTimeCallback) {
+            this.intervalId = setInterval(() => {
+                this.currentTime++;
+                printTimeCallback();
+            }, 1000);
+        } else {
+            this.intervalId = setInterval(() => {
+                this.currentTime++;
+            }, 1000);
+        }
+    }
 
-  getSeconds() {
-    // ... your code goes here
-  }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    getMinutes() {
 
-  stop() {
-    // ... your code goes here
-  }
+        return Math.floor(this.currentTime / 60);
 
-  reset() {
-    // ... your code goes here
-  }
+    }
 
-  split() {
-    // ... your code goes here
-  }
+    getSeconds() {
+        return this.currentTime % 60;
+    }
+
+    computeTwoDigitNumber(value) {
+        if (value < 10) {
+            return `0${value}`;
+        }
+        return `${value}`;
+    }
+
+
+
+    stop() {
+        clearInterval(this.intervalId);
+
+
+    }
+
+    reset() {
+
+        this.currentTime = 0;
+
+    }
+
+    split() {
+        // ... your code goes here
+    }
 }
