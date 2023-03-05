e2528f4ceb2234c08b5bf545227dc80602a3dca8
Ayoze Martin <ayozemt@gmail.com>
2023-03-05 13:32:08 +0100

    hasta Reset

--- src/chronometer.js
+++ src/chronometer.js
@@ -5,7 +5,7 @@ class Chronometer {
   }
 
   start(printTimeCallback) {
-    Chronometer.intervalId = setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.currentTime++;
       if (printTimeCallback) printTimeCallback();
     }, 1000);
@@ -20,18 +20,22 @@ class Chronometer {
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } else return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
