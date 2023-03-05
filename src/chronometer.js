21bc0dde32e23b3c05a575dd15df03d290fddd49
Javier Vivas <javier.vivas@icloud.com>
2023-03-05 15:07:08 +0100

    Faltan 3 puntos por completar

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime = 0,
+    this.intervalId = null
+  };
 
+//explicación en "JS | Async & Callbacks - Time to practice 2".
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    setInterval(()=>{
+      this.currentTime++;
+      if(printTimeCallback){
+          printTimeCallback();
+      }
+    }, 1000);
+  };
 
-  getMinutes() {
-    // ... your code goes here
-  }
+ getMinutes() {
+    if(this.currentTime === 0){
+      return 0;
+    }
+    return Math.floor(this.currentTime / 60);
+  };
 
   getSeconds() {
-    // ... your code goes here
-  }
+    return this.currentTime
+  };
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    if(value < 10){
+      return "0" + value.toString();
+    }
+      return value.toString();
+  };
 
   stop() {
-    // ... your code goes here
-  }
+    clearInterval(this.intervalId);
+  };
 
   reset() {
-    // ... your code goes here
-  }
+    return this.currentTime = 0;
+  };
 
   split() {
-    // ... your code goes here
-  }
-}
+  return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds())
+    
+  };
+};
