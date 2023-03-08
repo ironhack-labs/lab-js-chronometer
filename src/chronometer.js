281eb32be928dce53662907447d3abb60f666ce3
Jon <jon_ander_23@protonmail.com>
2023-03-08 08:57:20 +0100

    All done except last point of last iteration

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,45 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval ( () => {
+      if (printTimeCallback !== undefined) {
+        printTimeCallback();
+      }
+      return (this.currentTime += 1);
+      }, 1000)
+
   }
 
   getMinutes() {
-    // ... your code goes here
-  }
+    return (Math.floor(this.currentTime/60));
+    }
 
   getSeconds() {
-    // ... your code goes here
-  }
+return this.currentTime % 60;  }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return "0" + value.toString();
+    }
+    return value.toString();
   }
 
   stop() {
-    // ... your code goes here
+    return clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    if (this.getSeconds() > 60) {
+      this.split(':');
+    }
   }
 }
