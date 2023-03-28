f99e3c1462baf2925851945a4ae12d8ea0f04557
gunnar-miklis <gunnar.miklis@gmail.com>
2023-03-28 17:53:34 +0200

    Done: all iterations

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(
+      () => {
+        this.currentTime++;
+        if (printTimeCallback) printTimeCallback();
+      },
+      10
+    )
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor( this.currentTime / 6000 );
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor( this.currentTime / 100 )%60;
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return this.currentTime%100;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ("0" + value).slice(-2);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const mm = this.computeTwoDigitNumber( this.getMinutes() );
+    const ss = this.computeTwoDigitNumber( this.getSeconds() );
+    const SS = this.computeTwoDigitNumber( this.getCentiseconds() );
+
+    return `${mm}:${ss}.${SS}`
   }
 }
