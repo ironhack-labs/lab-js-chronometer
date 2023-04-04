08fc5a2fd1d31fba7448c616477956d53e0337ea
Aziya Nissam <aziyanissam94@gmail.com>
2023-04-04 15:41:37 +0200

    Solved Lab

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,14 +1,26 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      
+      if(printTimeCallback){
+        printTimeCallback(this.currentTime);
+
+      }
+      }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    let newVal = Math.floor(this.currentTime/60)
+     return newVal
   }
 
   getSeconds() {
--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,65 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      
+      if(printTimeCallback){
+        printTimeCallback(this.currentTime);
+
+      }
+      }, 1000);
+     
   }
+  
+    
 
   getMinutes() {
     // ... your code goes here
+     let newVal = Math.floor(this.currentTime/60)
+     return newVal
   }
 
   getSeconds() {
     // ... your code goes here
+    let secondGet = this.currentTime % 60;
+    return secondGet
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let newStr = value.toString()
+    if(newStr.length === 1){
+      return "0"+newStr
+    }
+    else{
+      return newStr
+    }
   }
 
   stop() {
     // ... your code goes here
+    return clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime=0;
+    
   }
 
   split() {
     // ... your code goes here
+    let new1=this.computeTwoDigitNumber(this.getMinutes())
+    let new2=this.computeTwoDigitNumber(this.getSeconds())
+    return `${new1}:${new2}`
+
   }
 }
