d1a9655090ab7d27ce83bd9b8ea53c5d4eb12caf
Lee Vang <vanglee.20@gmail.com>
2023-04-05 10:32:00 -0500

    formatted

--- src/chronometer.js
+++ src/chronometer.js
@@ -14,47 +14,51 @@ class Chronometer {
   }
 
   getMinutes() {
-    return Math.floor(this.currentTime/60)
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    return Math.floor(this.currentTime % 60)
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
-    let tempString = ''
-    tempString += value
+    let tempString = "";
+    tempString += value;
     if (tempString.length == 1) {
-      tempString = '0' + tempString
-      return tempString
+      tempString = "0" + tempString;
+      return tempString;
     } else {
-      return tempString
+      return tempString;
     }
 
   }
 
   stop() {
-      clearInterval(this.intervalId)
-    }
-  
+    clearInterval(this.intervalId);
+  }
 
+  
   reset() {
-    this.currentTime = 0
+    this.currentTime = 0;
   }
 
   split() {
-    let currentTimeString = '' + this.currentTime;
-    let minutesString = this.computeTwoDigitNumber(this.getMinutes(this.currentTime));
-    let secondsString = '';
+    let currentTimeString = "" + this.currentTime;
+    let minutesString = this.computeTwoDigitNumber(
+      this.getMinutes(this.currentTime)
+    );
+    let secondsString = "";
     if (this.currentTime >= 60) {
-      secondsString = this.computeTwoDigitNumber(this.getSeconds(this.currentTime));
+      secondsString = this.computeTwoDigitNumber(
+        this.getSeconds(this.currentTime)
+      );
     } else if (currentTimeString.length === 2) {
       secondsString = currentTimeString;
     } else {
-      secondsString = '0' + currentTimeString;
+      secondsString = "0" + currentTimeString;
     }
     let totalString = minutesString + ":" + secondsString;
     return totalString;
   }
-  
+
 }
