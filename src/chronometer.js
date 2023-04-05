a4e82f137325f24eca254be243ebbfdec9ac0425
Daniel Cornejo <contact@danielcornejo.dev>
2023-04-04 22:05:02 -0400

    Lab complete without bonuses

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    if (typeof printTimeCallback !== "function") {
+      printTimeCallback = () => {};
+    }
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      printTimeCallback(this.currentTime);
+    }, 1000);
   }
 
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    let num = value.toString();
+    
+    if(num.length === 1) {
+      return 0 + num
+    } else if (num.length === 2 ) {
+      return num
+    } else if (num.length > 2) {  
+      return num.slice(2, num.length - 2)
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
