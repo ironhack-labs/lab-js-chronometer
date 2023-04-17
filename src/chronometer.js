b98e81d919f0b37eb2f916e81b37d61b0505fe4e
Tomas Maria <tomaslbpmaria@hotmail.com>
2023-04-17 16:18:49 +0100

    Lab day 6 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
-  }
+      this.intervalId = setInterval(() => {
+          this.currentTime++;
+          if (printTimeCallback) {
+            printTimeCallback();
+          }
+      }, 1000);
+    }
 
   getMinutes() {
     // ... your code goes here
+    let minutes = Math.floor(this.currentTime/60);
+    return minutes;
   }
 
   getSeconds() {
     // ... your code goes here
+    let seconds = Math.floor(this.currentTime%60);
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value.toString.length===1){
+      return value.toString().padStart(2,"0");
+    }
+    return value.toString();
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime=0;
   }
 
   split() {
     // ... your code goes here
+      return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
