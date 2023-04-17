410916418796056ea608ef7703f039810c06e808
Niroj Gautam <gniroj964@gmail.com>
2023-04-17 17:49:59 +0100

    done chronometer

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,53 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) printTimeCallback();
+      this.currentTime += 1;
+    }, 1000);
   }
+    
+   
+  
 
   getMinutes() {
-    // ... your code goes here
+   let minutes = Math.floor(this.currentTime / 60)
+   return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = Math.floor(this.currentTime % 60)
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value === 0){
+      return '00';
+    }else if (value <10){
+      return '0'+ value.toString();
+    }else {
+      return value.toString();
+    }
+    
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    
   }
 
   reset() {
-    // ... your code goes here
+   this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
-  }
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+  
+  return minutes + ":" + seconds;
+ }
 }
