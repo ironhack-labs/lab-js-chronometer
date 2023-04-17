dc21b00592be1c7c3d6b3fca21b179cb4cc5199a
BernardoGuedesAbreu <bernardoabreu1991@gmail.com>
2023-04-17 16:41:54 +0100

    lab6 done

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(()=>{
+      this.currentTime++;
+        if(printTimeCallback){
+          printTimeCallback();
+        }
+        return console.log((this.currentTime%60))  
+    },10)
   }
 
   getMinutes() {
-    // ... your code goes here
-  }
+    return Math.floor((this.currentTime/6000))  }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(((this.currentTime/100)%60))
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime%100))
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value.length ===2){
+      return value.toString();
+    }else{
+    return value.toString().padStart(2,0)
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}.${this.computeTwoDigitNumber(this.getCentiseconds())}`;
+
+  
   }
 }
