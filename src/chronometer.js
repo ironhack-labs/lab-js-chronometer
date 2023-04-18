b992f3930d0462ae23fb60cf01dd86855de70560
Pedro de la Torre <pedrodltl@gmail.com>
2023-04-18 16:08:16 +0200

    Lab Ricardo y Pedro

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,37 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+      this.currentTime++
+    }, 1000);
+
+    return printTimeCallback
+
   }
 
   getMinutes() {
-    // ... your code goes here
+
+    if (this.currentTime === 0) {
+      return 0
+    }
+    return Math.floor(this.currentTime / 60)
   }
 
+
+
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0
+    }
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
