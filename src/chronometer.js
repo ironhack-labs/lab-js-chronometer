21cfa36f24c4ccf1975716c62d1f3bf12140190b
LourdesGonzalezT <l.gonzalezte@gmail.com>
2023-04-18 16:08:19 +0200

    done by Manuel & Lourdes

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,19 +1,37 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime <= 60) {
+      clearInterval(this.currentTime)
+      return this.currentTime
+
+    } else {
+      return this.currentTime - 60
+    }
+
   }
+
+
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
