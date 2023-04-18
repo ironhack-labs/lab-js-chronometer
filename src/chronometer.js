a68a1a2b2d0b4ae6149356522a32aadec4ba0e93
Carloslt <carloslt.proyectos@gmail.com>
2023-04-18 16:02:31 +0200

    slab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,45 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+      this.currentTime++
+    }, 1000)
+
   }
 
   getMinutes() {
     // ... your code goes here
+
+    return Math.floor(this.currentTime / 60)
+
   }
 
   getSeconds() {
     // ... your code goes here
+
+    return Math.floor(this.currentTime % 60)
+
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    if (value < 10) {
+      '0' + value
+    } else {
+      value
+    }
+
+    return String(value)
   }
 
   stop() {
