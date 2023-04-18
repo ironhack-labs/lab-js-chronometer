78b5832791fe110fcdd4d8d7afd3e89cf0599654
lortes <lortes95@gmail.com>
2023-04-18 16:07:42 +0200

    semi-done:(

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,32 +1,48 @@
+
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0,
+    this.intervalId = null 
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() =>{
+      this.currentTime++
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+    },1000)
+   // return intervalId
   }
+  
 
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime / 60)
   }
+  
 
   getSeconds() {
-    // ... your code goes here
+   return this.currentTime % 60
   }
-
+  
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    const numberString = value.toString();
 
+    if(numberString.length === 2){
+      return numberString
+    }else{
+      return "0"+numberString
+    }
+  }
+  
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
-
+  
   reset() {
     // ... your code goes here
   }
-
+  
   split() {
     // ... your code goes here
   }
