b4da1d38e318f62ce34339b38f00459f42808a25
Bernard Cardoso <be1.0@hotmail.com>
2023-04-28 23:59:42 +0100

    Doing first iterations

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,23 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    const starter = setInterval(() => {
+      this.currentTime++}, 1000);
+   
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = this.currentTime % 60000;
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60;
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
