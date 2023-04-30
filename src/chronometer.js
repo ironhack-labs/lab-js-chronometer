b5d6daf122382f583e5f57f508e248d8f4d697d5
daliaelena <dalia.geo89@gmail.com>
2023-04-29 18:04:08 -0600

    feature clock

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,54 @@
 class Chronometer {
   constructor() {
+
+    this.currentTime = 0;
+    this.intervalId =null;
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    // this.intervalId=setInterval(printTimeCallback,1000);
+    // return this.intervalId++
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) printTimeCallback()
+    }, 1000);
+
   }
+
 
   getMinutes() {
-    // ... your code goes here
-  }
+    return Math.floor(this.currentTime / 60)
+}
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+      if(value<10){
+        return `0${value}` 
+      }
+      
+      else {
+        return String(value);
+      }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+        this.currentTime =0;
   }
 
   split() {
-    // ... your code goes here
+
+    return this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds())
+    
   }
 }
