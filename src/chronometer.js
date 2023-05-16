d24fb7cbd8f4ef930b8f072e43ee5b23278a9c0d
pedroxpaulos <paulos.pedro@gmail.com>
2023-05-16 17:27:42 +0100

    All exercises done, only Bonus Iteration 9 to be done.

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+  
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) { printTimeCallback() };
+      this.currentTime++;
+    },1000);
+    }
+  
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
+    let result = "";
+    if (value < 10) {
+      result = `0${value}`;
+    } else {
+      result = `${value}`;
+    }
+    return result;
     // ... your code goes here
   }
 
+
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let mm = this.getMinutes();
+    let ss = this.getSeconds();
+    let time = this.computeTwoDigitNumber(mm) + ":" + this.computeTwoDigitNumber(ss);
+    return time;
   }
 }
