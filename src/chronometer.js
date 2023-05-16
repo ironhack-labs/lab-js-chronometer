62787d5e4faf2683d2b62433f9bf54b6427d7cec
Gretil <gretil_15@hotmail.com>
2023-05-16 17:38:03 +0200

    All iterations 1-8 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,59 @@
+/** @format */
+
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+	constructor(currentTime = 0, intervalId = null) {
+		// ... your code goes here
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+		this.currentTime = currentTime;
+		this.intervalId = intervalId;
+	}
 
-  getMinutes() {
-    // ... your code goes here
-  }
+	start(printTimeCallback) {
+		// ... your code goes here
+		this.intervalId = setInterval(() => {
+			this.currentTime++;
 
-  getSeconds() {
-    // ... your code goes here
-  }
+			if (printTimeCallback) {
+				printTimeCallback();
+			}
+		}, 1000);
+	}
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+	getMinutes() {
+		// ... your code goes here
 
-  stop() {
-    // ... your code goes here
-  }
+		return Math.floor(this.currentTime / 60);
+	}
 
-  reset() {
-    // ... your code goes here
-  }
+	getSeconds() {
+		// ... your code goes here
 
-  split() {
-    // ... your code goes here
-  }
+		return this.currentTime % 60;
+	}
+
+	computeTwoDigitNumber(value) {
+		// ... your code goes here
+		value = String(value);
+
+		if (value.length < 2) {
+			return "0" + value;
+		} else return value;
+	}
+
+	stop() {
+		// ... your code goes here
+		clearInterval(this.intervalId);
+	}
+
+	reset() {
+		// ... your code goes here
+		this.currentTime = 0;
+	}
+
+	split() {
+		// ... your code goes here
+		return `${this.computeTwoDigitNumber(
+			this.getMinutes()
+		)}:${this.computeTwoDigitNumber(this.getSeconds())}`;
+	}
 }
