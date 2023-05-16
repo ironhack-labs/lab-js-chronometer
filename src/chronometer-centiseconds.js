11d299842fb3e49c9c5878cc7ecb2402d5a60fae
Philippe Miossec <pmiossec@gmail.com>
2023-05-16 16:48:18 +0200

    Part2: Chronometer-centiseconds

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,57 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+    this.id = undefined;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.id = setInterval(() => {
+      this.currentTime++;
+    }, 10);
+
+    if(printTimeCallback !== undefined) {
+      this.id = setInterval(() => {
+        printTimeCallback();
+      }, 10);
+    }
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60_00);
   }
 
   getSeconds() {
     // ... your code goes here
+    return Math.floor((this.currentTime - this.getMinutes() * 60_00) /100);
   }
 
   getCentiseconds() {
     // ... your code goes here
+    return this.currentTime % 100;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return String(value).padStart(2, '0');
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.id);
   }
 
   reset() {
     // ... your code goes here
+    this.stop();
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}.${this.computeTwoDigitNumber(this.getCentiseconds())}`;
   }
 }
