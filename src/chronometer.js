17b41a13eb6a2b4b062e5de2ab2e0d82f4cbf644
Chiara888 <carolin@family-klose.de>
2023-05-16 19:25:13 +0200

    iterations done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if(printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);  
   } 
 
+
   getMinutes() {
-    // ... your code goes here
+    const minutesPassed = Math.floor(this.currentTime/60)
+    return minutesPassed;
   }
+
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime%60;
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+  
+  computeTwoDigitNumber(value) { 
+    if(value<10){
+      return`0${value}`
+    } else if (value>10){
+      return`${value}`
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+     
   }
 }
+
