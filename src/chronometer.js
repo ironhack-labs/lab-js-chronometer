03a74f38d918f5e5f4a1f1490035fea825e58b64
hymced <hymced@gmail.com>
2023-05-16 17:18:25 +0200

    done except bonus 9

--- src/chronometer.js
+++ src/chronometer.js
@@ -5,32 +5,45 @@ class Chronometer {
   }
 
   start(printTimeCallback) {
-    
+    this.intervalId = setInterval(() => { // use arrow function to access this object other with a function this will be global object
+      this.currentTime++
+      if (printTimeCallback) printTimeCallback()
+    }, 1000)
+    return this.intervalId
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
+    return this.currentTime - this.getMinutes() * 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ("00" + value).slice(-2) // adds 2 leading zeros no matter if value has 1 or 2 digits, then extracts from the end the last 2 characters
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime  =0
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds())
   }
+
 }
 
 const chrono = new Chronometer();
+chrono.start(hello)
+
+setTimeout(() => console.log(chrono.split()), 5000)
+
+function hello() {
+  console.log("hello")
+}
