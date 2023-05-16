9dd113778520372edcba62d547282a095595c0ec
hymced <hymced@gmail.com>
2023-05-16 18:08:28 +0200

    done with bonus 9 centiseconds

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,62 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0 // now stores 10 milliseconds = 1 centisecond instead of seconds
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => { // use arrow function to access this object other with a function this will be global object
+      this.currentTime++
+      if (printTimeCallback) printTimeCallback()
+    }, 10) // change here
+    return this.intervalId
   }
 
+  // 61 001 ms // % 1000 = 1
+  // 61 sec 1 ms
+  // 1 min 1 sec 1 ms
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 100 / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    // 2 solutions (the 2nd is never reached so no need to comment it out)
+    return Math.floor(this.currentTime % (100 * 60) / 100)
+    return Math.floor(this.currentTime / 100) - this.getMinutes() * 60
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    // 2 solutions (the 2nd is never reached so no need to comment it out)
+    return this.currentTime % 100
+    return this.currentTime - this.getMinutes() * 60 * 100 - this.getSeconds() * 100
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ("00" + value).slice(-2) // adds 2 leading zeros no matter if value has 1 or 2 digits, then extracts from the end the last 2 characters
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime  =0
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds()) + '.' + this.computeTwoDigitNumber(this.getCentiseconds())
   }
 }
+
+// bonus iteration 9 - centiseconds
+// chronometer-centiseconds.js
+// requires to manually change SpecRunner.html to change Jasmine's js test files
+// and also index.html to visualize the chronometer watch in Live Server
+
+const chrono = new Chronometer();
+
+chrono.currentTime = 510
+console.log(chrono.split()) // 00:05.10
+chrono.currentTime = 1725
+console.log(chrono.split()) // 00:17.25
--- src/chronometer.js
+++ src/chronometer.js
@@ -8,7 +8,7 @@ class Chronometer {
     this.intervalId = setInterval(() => { // use arrow function to access this object other with a function this will be global object
       this.currentTime++
       if (printTimeCallback) printTimeCallback()
-    }, 1000)
+    }, 1000) // *the first execution starts AFTER the delay*
     return this.intervalId
   }
 
@@ -17,6 +17,7 @@ class Chronometer {
   }
 
   getSeconds() {
+    // 2 solutions (the 2nd is never reached so no need to comment it out)
     return this.currentTime % 60
     return this.currentTime - this.getMinutes() * 60
   }
@@ -42,7 +43,9 @@ class Chronometer {
 const chrono = new Chronometer();
 chrono.start(hello)
 
-setTimeout(() => console.log(chrono.split()), 5000)
+setTimeout(() => console.log(chrono.split()), 5100) 
+// 5000 returns 00:04 (the timeout function call occurs a little bit too soon)
+// 5100 returns 00:05 ok!
 
 function hello() {
   console.log("hello")
