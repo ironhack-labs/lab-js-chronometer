9f07cb72597f1a77fe71e8b5c5204124c7748b95
JCouderc87 <jeremy.couderc@yahoo.fr>
2023-05-16 19:18:01 +0200

    some success

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,15 +1,30 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+  
 
   start(printTimeCallback) {
     // ... your code goes here
-  }
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        if (printTimeCallback) {
+        printTimeCallback()}
+      }, 1000);
+  
+   }
+
+    
+
 
   getMinutes() {
     // ... your code goes here
-  }
+
+  
+}
+  
 
   getSeconds() {
     // ... your code goes here
