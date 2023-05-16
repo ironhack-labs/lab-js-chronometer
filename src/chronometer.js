147d3ed511ad3900a3f2fec3100c848612e1c2ea
Joao Faria <joaorafaria@gmail.com>
2023-05-16 16:19:38 +0100

    iteration 4 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,17 +1,25 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      this.currentTime++;
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
+    return Math.floor(this.currentTime % 60);
     // ... your code goes here
   }
 
