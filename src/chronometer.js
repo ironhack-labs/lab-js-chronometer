8111eff8d843517c449f13eaf8567d683cf4f84b
Lina Zerkly <zerkly.lina@gmail.com>
2023-05-16 20:40:42 +0200

    8 iterations

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,54 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+    this.currentTime +=1;
+    if (printTimeCallback) {
+      printTimeCallback();
+    } 
+}, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
-  }
+  // 1- calculate how minutes have elapsed - 2 divide the currentTime / 60 + mathfloor method
+return Math.floor(this.currentTime / 60);  
+}
 
   getSeconds() {
-    // ... your code goes here
-  }
+    this.seconds = 0
+    // module operator (current time % 60)
+    return this.currentTime % 60; }
+    
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let twoDigit = value.toString() // turn a number to a string
+  if (value < 10 ) {
+    return "0" + twoDigit
+  }
+  else {
+    return twoDigit
+  }
   }
 
+
   stop() {
-    // ... your code goes here
+    // clearInterval
+  clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
-  }
+    // currentTime = 0
+  this.currentTime = 0
+}
 
   split() {
-    // ... your code goes here
-  }
+    // creating 2 variables to store the minutes and the seconds + returning with backticks
+ const minutes = this.computeTwoDigitNumber(this.getMinutes());
+ const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
+}
 }
