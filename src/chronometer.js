6883b6f3b8f63677beef1d1ff91bb04962717208
Dok <pierredok@outlook.fr>
2023-05-16 17:44:58 +0200

    done without Bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -16,23 +16,30 @@ class Chronometer {
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+  computeTwoDigitNumber(value){
+    const stringValue = value.toString();
+
+    if(stringValue.length === 1){
+      return "0" + value
+
+    } else if(stringValue.length === 2){
+      return '' + value
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
