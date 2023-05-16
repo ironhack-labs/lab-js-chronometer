68168a487dc369dd99da04be0ddeeb8cd60272db
Eirik Klebo <eiriktobias@gmail.com>
2023-05-17 00:26:18 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,43 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    setInterval(function (printTimeCallback) {
+      this.currentTime++;
+      return this.currentTime;
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+
+    return this.currentTime % 60;
   }
 
-  computeTwoDigitNumber(value) {
+  computeTwoDigitNumber(number) {
     // ... your code goes here
+    return number > 9 ? "" + number : "0" + number;
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(null);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
