9bfba0f6bf622a8a9fa926b03441622e0342d4dd
Ola Bonati <olabonati@gmail.com>
2023-05-16 23:27:32 +0200

    except iteration 5

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,60 @@
 class Chronometer {
   constructor() {
+  this.currentTime = 0;
+  this.intervalId = null;
+  
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+     this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback(); 
+      }}, 1000);
+    
   }
+    // ... your code goes here
+  
+  
 
   getMinutes() {
+    let numberofMinutes = Math.floor(this.currentTime/60);
+    return numberofMinutes;
     // ... your code goes here
   }
 
   getSeconds() {
+
+    let numberofSeconds = this.currentTime % 60;
+    return numberofSeconds;
+   
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+
+    value = this.currentTime    
+    const string = value.toString;
+    string.padStart(2,"0");
+
+      return ;
     // ... your code goes here
+
   }
 
   stop() {
+    clearInterval(this.intervalId);
     // ... your code goes here
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime= 0;
   }
 
   split() {
+
     // ... your code goes here
   }
 }
