e2874d4ca0b514461d87b561e415ccd535dae585
disaacjm <disaacjm@gmail.com>
2023-05-16 17:05:12 +0100

    lab almost done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
-
   start(printTimeCallback) {
-    // ... your code goes here
+   setInterval(() => {
+    this.currentTime++; 
+   }, 1000);
+   
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60) 
   }
 
   getSeconds() {
-    // ... your code goes here
-  }
+    return this.currentTime - this.getMinutes() * 60
+    }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ("00" + value)
   }
 
   stop() {
-    // ... your code goes here
   }
+
+  stop() {
+  }
+
+  stop() {
+clearInterval(this.currentTime)
+}
 
   reset() {
-    // ... your code goes here
-  }
+    this.stop
+    this.currentTime = 0;
+    }
 
   split() {
-    // ... your code goes here
   }
 }
