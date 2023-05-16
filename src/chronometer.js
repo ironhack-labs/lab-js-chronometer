87bf0e7dcbb3b8772a82f67f5cf675a00e0413fb
mk3346 <mk3346@hotmail.de>
2023-05-16 16:51:49 +0200

    Lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,55 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) {
+        printTimeCallback(this.currentTime);
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const stringValue = value.toString();
+    if (stringValue.length === 1) {
+      return "0" + stringValue;
+    }
+    return stringValue;
   }
+  
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    const splitMinutes = this.computeTwoDigitNumber(this.getMinutes());
+    const splitSeconds = this.computeTwoDigitNumber(this.getSeconds());
+    return splitMinutes + ':' + splitSeconds;
   }
 }
