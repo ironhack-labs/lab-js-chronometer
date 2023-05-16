3e930135e3a7c3606c86aa024408178939e50e4d
linhvnde <linh.nh.tyty@gmail.com>
2023-05-16 18:20:52 +0200

    LAB -  JS Chronometer - without iteration 9

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,60 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60);
+    return minutes;
   }
 
   getSeconds() {
     // ... your code goes here
+    const seconds = this.currentTime % 60;
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let twoDigitNumber = "";
+
+    if (value >= 0 && value < 10) {
+      twoDigitNumber = `0${value}`;
+    } else {
+      twoDigitNumber = `${value}`;
+    }
+    return twoDigitNumber;
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    let timeStamp = `${minutes}:${seconds}`;
+    return timeStamp;
   }
 }
