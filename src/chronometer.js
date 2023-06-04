9be5c8038cd5296f902ba67e097051064fe3adf6
robopecha <julikolb@gmail.com>
2023-06-04 22:41:40 +0200

    not done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,10 +1,14 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      printTimeCallback();
+    }, 1000);
   }
 
   getMinutes() {
