b31be7b6b2019215a48492600ab7159454dd1c46
ssandlin12 <ssandlin12@gmail.com>
2023-06-06 19:08:10 +0200

    finished

--- src/chronometer.js
+++ src/chronometer.js
@@ -26,21 +26,31 @@ class Chronometer {
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let twoDigits = value.toString();
+
+    if (value < 10) {
+      twoDigits = "0" + twoDigits;
+    }
+    return twoDigits;
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    
   }
 }
