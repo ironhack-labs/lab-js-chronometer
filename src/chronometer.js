9493e06fbc0b05b0bf11649dda19856c4d18ac18
ssandlin12 <ssandlin12@gmail.com>
2023-06-06 17:25:04 +0200

    first commit

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,14 +1,27 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
-  }
+
+
+      this.intervalId = setInterval(() => {
+        if (printTimeCallback !== undefined || printTimeCallback)
+        {
+        printTimeCallback() }
+
+          this.currentTime++;
+        }, 1000);
+    }
+
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
