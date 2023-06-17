242e884422349c7d3f3bd4d9133eb1dba49f6123
Naiim Taefy <naiim.tj@gmail.com>
2023-06-17 13:39:30 +0200

    feat: Final exerce

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,34 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
+  constructor(currentTime = 0, intervalId = null) {
+    this.currentTime = currentTime;
+    this.intervalId = intervalId;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime +=1;
+      printTimeCallback && printTimeCallback()
+    }, 1000)
   }
 
-  getMinutes() {
-    // ... your code goes here
+  getMinutes() { return Math.floor(this.currentTime / 60)
   }
 
-  getSeconds() {
-    // ... your code goes here
+  getSeconds() { return this.currentTime - this.getMinutes()*60
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+  computeTwoDigitNumber(value) { return value < 10 ? `0${value}` : `${value}`
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
