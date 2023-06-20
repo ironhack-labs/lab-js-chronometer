5bfa0af411c3800a2e0e4f74a29731758d77daa3
Jaime Toro <zomo505@gmail.com>
2023-06-20 18:04:56 +0200

    Up to Bonus Iteration 8

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,47 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(()=> {
+      this.currentTime +=1;
+      if (printTimeCallback) {printTimeCallback()};
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60) ;
   }
 
   getSeconds() {
     // ... your code goes here
+    const secondsElapsed = this.currentTime % 60
+    return secondsElapsed
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const padded = ("0" + value).slice(-2) 
+  return padded
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
