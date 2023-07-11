0fa141cbf825809ae51c1ec746dd1089df1756d7
Agathe Lurquin <agathe.lurquin@essca.eu>
2023-07-11 15:33:09 +0200

    all done exept from lost methid

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+      this.currentTime = 0;
+      this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if(!printTimeCallback){
+      } else {
+        printTimeCallback()
+      }
+
+    }, 1000);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime%60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    value = value.toString()
+    if(value.length<2){
+      return `0${value}`
+    }
+    return value
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0;
   }
 
   split() {
+    console.log(typeof this.getMinutes(), this.currentTime, this.getMinutes(), this.getSeconds())
+    // console.log(`${this.getMinutes()}:${ this.getSeconds()}`)
     // ... your code goes here
   }
 }
