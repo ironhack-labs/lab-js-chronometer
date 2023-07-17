f660efa9bc3620980fe5a7c40338f432d41b1aef
Tiago Rodrigues <tiagomgr97@gmail.com>
2023-07-17 16:03:42 +0100

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,54 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    // Current time
+    this.currentTime = 0;
+    // Interval Id
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(()=>{
+        this.currentTime++;
+        if(printTimeCallback){
+          printTimeCallback();
+        }
+    }, 1000);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor((this.currentTime/60));
   }
 
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime%60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    let newValue = "";
+    if(value === 0){
+        newValue = "00";
+      }else if(value < 10){
+        newValue += "0" + value.toString();
+      }else{
+        newValue = value.toString();
+      }
+      return newValue;
   }
 
   stop() {
-    // ... your code goes here
+    
+    clearInterval(this.intervalId);
+
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
   }
 }
