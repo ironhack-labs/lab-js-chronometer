3e6882fadbe937cade71054cdcacfad1b798a687
Claudia Alvarez Posada <daibhaith@hotmail.com>
2023-07-19 16:23:24 +0200

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    const intervalID = setInterval(() => {
+      printTimeCallback()
+      this.currentTime += 1
+    }, 1000)
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60)
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    if (value >= 0 && value < 10) {
+      return (`0${value}`)
+
+    } else {
+      return (`${value}`)
+
+    }
+
   }
 
+
   stop() {
-    // ... your code goes here
+
+    clearInterval(this.intervalId)
+
   }
 
   reset() {
-    // ... your code goes here
+
   }
 
   split() {
-    // ... your code goes here
+
   }
 }
