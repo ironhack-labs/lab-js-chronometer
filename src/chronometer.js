888000a7b7fbc7e1c3293f66901e9232b01e1251
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-08-08 22:10:35 +0100

    almost solved ... i guess the nap didnt quite help

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,41 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0 ;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    setInterval((intervalId) =>{this.currentTime++},1000)
+    if(printTimeCallback === true){
+      return  printTimeCallback(()=>{},1000)} 
   }
+  
 
   getMinutes() {
     // ... your code goes here
+   return Math.floor(this.currentTime /60) ; 
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+   
   }
 
   stop() {
     // ... your code goes here
+  clearInterval(this.currentTime)
   }
 
   reset() {
     // ... your code goes here
+    return this.currentTime = 0  
   }
 
   split() {
