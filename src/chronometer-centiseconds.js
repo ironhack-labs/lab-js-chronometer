57e2c199ba383c8f74dfa106b5c23c7e5ab443ab
Sandrine Manel <sandrine.manelgmail.com>
2023-08-08 17:31:38 +0200

    Done

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() =>
+    {
+      this.currentTime++;
+      if(typeof printTimeCallback==='function'){
+        printTimeCallback();
+      }
+    }
+    ,10)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/6000)
+
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime%6000) /100)
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    console.log(this.currentTime%100);
+    return this.currentTime%100
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value < 9)
+    { return `0${value}`}
+    else { return `${value}`}
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes())+':'+this.computeTwoDigitNumber(this.getSeconds())+'.'+this.computeTwoDigitNumber(this.getCentiseconds());
   }
 }
