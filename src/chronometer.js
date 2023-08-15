682dd91a794ab273c38d714de5892c675fc5e069
shani <shani@shanis-MacBook-Pro.local>
2023-08-15 15:43:51 +0200

    Add chrono lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if(printTimeCallback) {
+        printTimeCallback();
+      } 
+      this.currentTime++;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+     if(`${value}`.length === 1) {
+       return `0${value}`
+    }  
+    return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+     this.currentTime = 0;
   }
-
   split() {
-    // ... your code goes here
+    const mins = this.computeTwoDigitNumber(this.getMinutes());
+    const sec = this.computeTwoDigitNumber(this.getSeconds());
+    return `${mins}:${sec}`
   }
 }
