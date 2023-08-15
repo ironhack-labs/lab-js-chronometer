deb77825d40f98ba38eff6911d0293da973782bb
Da-vinia <daviniatosco@gmail.com>
2023-08-15 22:03:23 +0200

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+
+ //  should increment by 1 the currentTime property every 1 second
+// should invoke the passed printTimeCallback every 1 second
 
   start(printTimeCallback) {
-    // ... your code goes here
+ 
+  this.intervalId = setInterval(() => {
+    this.currentTime ++;
+    if(printTimeCallback){
+      printTimeCallback()
+    }
+  },1000)
+
+  return this.intervalId
+
   }
 
+  
   getMinutes() {
-    // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60);
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    // const seconds = this.currentTime - minutes * 60;
+    const seconds = this.currentTime % 60;
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    // should always return a string of length 2
+    // getminutes and getsec should return 2 digits and transform to a string 
+    // if the digit is single should add 0 Slice method
+   return  ("0" + this.getMinutes()).slice(-2) && ("0" + this.getSeconds()).slice(-2)
   }
 
   stop() {
-    // ... your code goes here
-  }
+
+  if (this.currentTime !== 0) {
+  return clearInterval(this.intervalId);
+} 
+    
+ clearInterval(this.intervalId);
+
+}
 
   reset() {
-    // ... your code goes here
+     this.currentTime = 0;
   }
 
   split() {
