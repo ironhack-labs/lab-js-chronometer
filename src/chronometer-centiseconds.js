cf7cf2e4598440cc33f9c50578961fd0a6e169cf
John Doe <johndoe@example.com>
2023-08-15 18:46:48 +0200

    done

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,53 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+     this.intervalId = setInterval (() => {
+    
+        this.currentTime +=1;
+        if (printTimeCallback){
+          printTimeCallback()
+        }
+      }
+      , 1000)}
+  
 
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime/60);
+  
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime%60;
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime%1)*100);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+
+    return  `0${value}`; 
+    } return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+     
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+     this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+
+    let secondForm = this.computeTwoDigitNumber(this.getSeconds)
+    let minuteForm = this.computeTwoDigitNumber(this.getMinutes)
+    return finalForm = `${minuteForm} : ${secondForm}`
   }
 }
