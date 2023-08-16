5c1f779c4730cd8985c7991aab08426d04cc1e1b
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-16 09:04:16 +0200

    wip

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,23 +1,44 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;   
+    this.intervalId = null; //not interval has been estarted yet
   }
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+  start(printTimeCallback) { // the start method printTimeCallback function as an argument
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
 
+      if (printTimeCallback) {
+        printTimeCallback(this.currentTime);
+      }
+    }, 1000);
+  }
   getMinutes() {
-    // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60); 
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
-  }
+    // const seconds = this.minutes % 60;
+    // return seconds;
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    }
+
+    const seconds = this.currentTime % 60;
+    return seconds;
   }
+  
+
+ 
+  computeTwoDigitNumber(value) {
+    // const twoDigit = String(value).padStart(2, '0');
+    // return twoDigit;
+
+    const twoDigit = ('0' + value).slice(-2);
+    return twoDigit;
+    }
 
   stop() {
     // ... your code goes here
