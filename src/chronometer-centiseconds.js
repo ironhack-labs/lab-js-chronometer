0eb0021ed5c83812f5955207e416faa9302a6afe
Hector Hernandez <hectorahbravo@gmail.com>
2023-10-10 21:12:32 +0200

    lab-js-chronometer completed

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,62 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 10);
   }
 
   getMinutes() {
     // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 6000);
+    return minutes;
   }
 
   getSeconds() {
     // ... your code goes here
+    let seconds = Math.floor((this.currentTime / 100) % 60);
+    return seconds;
   }
-
   getCentiseconds() {
     // ... your code goes here
+    let centiseconds = this.currentTime % 100;
+    return centiseconds;
   }
 
   computeTwoDigitNumber(value) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return String(value);
+
     // ... your code goes here
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(
+      this.getSeconds()
+    )}.${this.computeTwoDigitNumber(this.getCentiseconds())}`;
   }
 }
