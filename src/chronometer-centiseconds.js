67cb0f505536174bf47e6f5adc5afa73d82ee6bb
Victor Junquera <victorjunquera@eventalista.es>
2023-10-11 18:31:06 +0200

    lab done

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,54 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 10)
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 6000)
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = Math.floor((this.currentTime / 100) % 60)
+    return seconds
   }
-
   getCentiseconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 100)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`
+    }
+    return `${value}`
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    //clearInterval para el intervalo
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const centiSeconds = this.computeTwoDigitNumber(this.getCentiseconds())
+
+    return `${minutes}:${seconds}.${centiSeconds}`;
   }
 }
