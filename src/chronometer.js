6e755f379eb77dc16d360f190fbe8976d389ce50
RodriguezJoseMaria <143606586+RodriguezJoseMaria@users.noreply.github.com>
2023-10-15 22:52:21 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,57 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+    // this.printTimeCallback = printTimeCallback;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+        this.currentTime++;
+        if (this.printTimeCallback) {
+            this.printTimeCallback(this.currentTime);
+        }
+      }, 1000);{
+      }
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutGet = Math.floor(this.currentTime / 60);
+    return minutGet;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let secondGet = Math.floor(this.currentTime % 60);
+    return secondGet;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const myNumber = 3;
+
+     function printTwoDigits(n) {
+       if (n >= 2) {
+       return '0' + n; 
+     } else {
+       return '' + n;
+     }
+  }
+     const twoDigits = printTwoDigits(myNumber);
+     return twoDigits;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.currentTime);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+   let digitNewNumber = this.computeTwoDigitNumber.split('');
+   function secondMinut () {
+    return this.getMinutes()  + ":" + this.getSeconds();
+   }
   }
 }
