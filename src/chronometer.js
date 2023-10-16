1d8eb50fd6f8a6d1ffb82db956c339bad81e244e
Gabriel Fernandez <gfernandezareas@gmail.com>
2023-10-16 11:14:48 +0200

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+    
+    if(printTimeCallback) {
+      printTimeCallback(this.currentTime);
+    }
+    this.currentTime += 1;
+    console.log(this.currentTime);
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return ("0" + value);
+    } else {
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+
   }
 }
+
+const myChronometer = new Chronometer;
+console.log(myChronometer.start());
