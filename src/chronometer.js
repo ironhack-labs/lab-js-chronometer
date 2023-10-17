39fb1468fb3a24b6981e9fbef4a092664b0ce1a1
angel060404 <angel.s.p2064@gmail.com>
2023-10-17 16:25:25 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,23 +1,48 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) printTimeCallback()
+    }, 1000
+    )
+
+
   }
 
   getMinutes() {
     // ... your code goes here
+
+    this.getMinutes = this.currentTime / 60
+    return Math.floor(this.getMinutes)
   }
 
   getSeconds() {
     // ... your code goes here
+    this.getSeconds = this.currentTime % 60
+    return this.getSeconds
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value < 10) {
+      value = "0" + value
+
+
+
+      return value
+    } else {
+      value = "" + value
+      return value
+    }
   }
+  //
 
   stop() {
     // ... your code goes here
