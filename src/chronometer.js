97732d88daa7f73c929b88ec9f88e79cd09ee2e0
Pablo Portillo <paabloportillo@gmail.com>
2023-10-17 15:54:09 +0200

    Hasta bonus 8

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,46 @@
 class Chronometer {
+
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1
+      if (printTimeCallback) printTimeCallback()
+    }, 1000);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const valueString = value.toString()
+    if (valueString.length === 1) {
+      return '0' + valueString
+    } else {
+      return valueString
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    const mins = this.computeTwoDigitNumber(this.getMinutes())
+    const secs = this.computeTwoDigitNumber(this.getSeconds())
+    return `${mins}:${secs}`
   }
 }
