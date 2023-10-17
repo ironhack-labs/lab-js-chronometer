9622121f7ff18eb25dd645d2d3a13381bdc8c367
FaresQaddoura29 <faresqaddoura@gmail.com>
2023-10-17 17:02:48 +0100

    Solved lab

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,72 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    const timer = setInterval(()=>{
+      this.currentTime++;
+        console.log(this.getSeconds());
+
+      this.intervalId = timer;
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+    }, 10)
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 6000)
+    if(this.intervalId<0){
+      return 0
+    }
+    else{
+      return minutes
+    }
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = (this.currentTime % 6000);
+    if(this.currentTime<0){
+      return 0
+    }
+    else{
+      return seconds
+    }
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    const cs = this.currentTime % 60
+    if(this.currentTime<1){
+      return 0
+    }
+    else{
+      return cs
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value<10){
+      return "0" + value;
+    }
+    else{
+      return value.toString()
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0;
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes())
+    const seconds = this.computeTwoDigitNumber(this.getSeconds())
+    const centi = this.computeTwoDigitNumber(this.getCentiseconds())
+    return minutes + ":" + seconds + "." + centi;
   }
 }
