d6aa208cdbe624490cb7eec5c3b7925f48a17da0
Marco Salvoni <marco.salvoni8@gmail.com>
2023-10-24 19:11:09 +0200

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,55 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if(typeof printTimeCallback === 'function'){
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    let currentMin = Math.floor(this.currentTime / 60)
+    return currentMin
   }
 
   getSeconds() {
     // ... your code goes here
+    let currentSec = this.currentTime % 60
+    return currentSec
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let valueString = value.toString();
+
+    if(valueString.length <= 1){
+      return `0${valueString}`
+    } else {
+      return valueString
+    }
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return '' + this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds());
   }
 }
