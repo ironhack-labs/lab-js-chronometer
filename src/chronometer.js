557ef3865b2aa90944a327e16982e2f78d24d4d0
yvzbly <yavuzbalay90@gmail.com>
2023-10-24 19:13:22 +0200

    WDFTOCT2023 Yavuz Balay

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if(typeof printTimeCallback ==='function'){
+      this.intervalId = setInterval(()=>{
+        this.currentTime += 1;
+        printTimeCallback()
+      }, 1000)
+    }else{
+      this.intervalId = setInterval(()=>{
+        this.currentTime += 1;
+      }, 1000)
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return this.currentTime / 60 | 0
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime %60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value < 10){
+      return "0" + value
+    }else{
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    
   }
 }
