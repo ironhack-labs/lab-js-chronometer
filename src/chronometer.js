0c922835b6d4c855b90fc987817af4a9c1002126
Laura Lauria <lauralauria@Air-von-Laura.local>
2023-10-24 17:53:20 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null; 
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000)
   }
+  /*start(printTimeCallback) {
+   this.intervalId = setInterval (()=> {
+    printTimeCallback && printTimeCallback()
+    this.currentTime++
+   },1000) 
+   
+  }
+  */
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor (this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor (this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let toString = value.toString()
+    if (toString.length === 1) {
+      return "0" + toString;
+      }
+      if (toString === 0) {
+        return "00";
+      }
+      return toString
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let splitMinutes = this.computeTwoDigitNumber(this.getMinutes()).toString();
+    let splitSeconds = this.computeTwoDigitNumber(this.getSeconds()).toString();
+    return splitMinutes + ":" + splitSeconds
   }
 }
