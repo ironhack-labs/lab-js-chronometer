8656105a3bf89c474e149881e5d361ec8f038e81
Ensha C Latheef <enshalatheef@gmail.com>
2023-10-24 12:38:47 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,65 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime=0
+    this.intervalId=null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    
+    const incrementTime = () => {
+      this.currentTime++;
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+    }
+
+    const intervalInMilliSec = 1000;
+    
+    this.intervalId = setInterval(incrementTime, intervalInMilliSec);
+   
   }
 
   getMinutes() {
-    // ... your code goes here
+    let count = this.currentTime / 60;
+    let minuteCount = Math.floor(count)
+    
+    return minuteCount;
   }
-
+  
   getSeconds() {
-    // ... your code goes here
+      // ... your code goes here
+      let getSecond = this.currentTime  % 60 
+      return getSecond;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const strNum=value.toString();
+    if (strNum.length===2) {
+      return strNum;
+    } else {
+      return `0${strNum}`;
+    }
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0    
   }
 
   split() {
     // ... your code goes here
+    const mins = this.getMinutes()
+    const secs= this.getSeconds()
+    const strMins=this.computeTwoDigitNumber(mins)
+    const strSecs=this.computeTwoDigitNumber(secs)
+    return `${strMins}:${strSecs}`
   }
+
 }
