c39c8e0ab006ebfdc271c0bd166d5e5cb2a3b3d3
Irene <amoreperdesi@gmail.com>
2023-12-12 15:07:08 +0100

    Added iterations 1,2,3

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,14 +1,23 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    setInterval(() => {
+      if (typeof printTimeCallback === "function") {
+        printTimeCallback()
+      }
+      return (this.currentTime ++);
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
