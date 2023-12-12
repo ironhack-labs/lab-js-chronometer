398350b6c91bd4cc6f1b0060446e477ea3147590
Eleanor Pitcher <eleanorpitcher@gmail.com>
2023-12-12 15:56:40 +0100

    lab done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (typeof printTimeCallback === "function"){
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    const numberMinutes = Math.floor(this.currentTime/60)
+    return numberMinutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    const numberSeconds = Math.floor(this.currentTime%60)
+    return numberSeconds
   }
 
+
+  // if there is one number, add a 0 to the string
+  // convert the number to a string first, then use slice method
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10){
+      let strValue = value.toString()
+      let newValue = '0' + strValue;
+      return newValue.toString()
+
+    } else {
+      return value.toString()
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
+    return clearInterval
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${getMinutes()} + ':' ${getSeconds(this.computeTwoDigitNumber)}`
   }
 }
