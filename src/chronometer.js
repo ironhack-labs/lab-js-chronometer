74d709d528bbfdfd422db85ab1eb1cdc8ff25738
Aron <emmanuel.aron@gmail.com>
2024-01-25 20:52:19 +0100

    First commit lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,22 +1,36 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    
+    this.intervalId = setInterval(()=>{
+      this.currentTime++
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let twoDigit = String(value).slice(0,2);
+    console.log('slice ',twoDigit)
+    return twoDigit
   }
 
   stop() {
