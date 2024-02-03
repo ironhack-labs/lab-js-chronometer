f5a08fe6b6bf3c7ba9e7d31f17418a16673cbfef
guzegui <guzmanegui@gmail.com>
2024-02-03 15:58:09 +0100

    Done Iteration 8 BONUS

--- src/chronometer.js
+++ src/chronometer.js
@@ -37,7 +37,7 @@ class Chronometer {
   }
 
   split() {
-    // ... your code goes here
+   return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
 
